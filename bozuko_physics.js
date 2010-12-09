ab759bdb22f316dcc25d838b40c817bbed8c56a4
Andrew Stone <andrew.j.stone.1@gmail.com>
2010-12-09 03:54:06 -0500

    updated physics code to add collision response. Stuff is still buggy.

--- bozuko_physics.js
+++ bozuko_physics.js
@@ -1,60 +1,212 @@
-function Point(x,y) {
-    this.x = x;
-    this.y = y;
-}
+// A lot of the code in this file is based off Olivier Renault's PollyColly
 
-function Projection(min, max) {
+function Interval(min, max) {
     this.min = min;
     this.max = max;
 }
 
-// All sprites are rectangles.
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+
+    this.plus = function(v) {
+	return new Vector(this.x + v.x, this.y + v.y);
+    };
+    
+    this.minus = function(v) {
+	return new Vector(this.x - v.x, this.y - v.y);
+    };
+
+    this.timesScalar = function(n) {
+	return new Vector(this.x * n, this.y * n);
+    };
+
+    this.perp = function() {
+	return new Vector(-y, x);
+    };
+
+    this.length = function() {
+        return Math.sqrt(this.x*this.x + this.y*this.y);
+    };
+
+    this.dot = function(v) {
+	return this.x*v.x + this.y*v.y;
+    };
+
+    this.cross = function(v) {
+	return this.x * v.y - this.y *v.x;
+    };
+    
+    this.normalize = function() {
+	var l = length();
+	if (l != 0) {
+	    this.x = this.x / l;
+	    this.y = this.y / l;
+	}
+    };
+
+    this.angle = function(v) {
+	var dp = this.dot(v);
+	var cp = this.cross(v);
+	return Math.atan2(cp, dp);
+    };
+
+    this.rotate = function(angle) {
+	var tx = this.x;
+	this.x = this.x * Math.cos(angle) - this.y * Math.sin(angle);
+	this.y = tx * Math.sin(angle) + this.y * Math.cos(angle);
+    };
+}
+
+function Polygon(vertices) {
+    this.vertices = vertices;   
+    
+    this.rotate = function(angle) {
+	var i; 
+	for (i = 0; i < this.vertices.length; i++) {
+	    this.vertices[i].rotate(angle);
+	}
+    };
+
+    this.translate = function(x, y) {
+	var i; 
+	for (i = 0; i < this.vertices.length; i++) {
+	    this.vertices[i].x += x;
+	    this.vertices[i].y += y;
+	}
+    };
+
+    this.plus = function(v) {
+	var i;
+	for (i = 0; i < this.vertices.length; i++) {
+	    this.vertices[i] = this.vertices[i].plus(v);
+	}
+    };
+
+    this.minus = function(v) {
+	var i;
+	for (i = 0; i < this.vertices.length; i++) {
+	    this.vertices[i] = this.vertices[i].minus(v);
+	}
+    };
+    
+    this.intersect = function(poly) {
+	var info = new CollisionInfo();
+	info.mtdLenSquared = -1; // flag mtd as invalid
+	var i, j;
+	var v0, v1;
+	var edge;
+	var axis;
+
+	// test separation axes of this
+	for (i = 0, j = this.vertices.length-1; i < this.vertices.length; j=i, i++) {
+	    v0 = this.vertices[j];
+	    v1 = this.vertices[i];
+	    edge = v1.minus(v0);
+	    axis = edge.perp();
+	    if (this.separatedByAxis(axis, poly, info)) {
+		return info;
+	    }
+	}
+
+	// test separation axes of poly	
+	for (i = 0, j = poly.vertices.length-1; i < poly.vertices.length; j=i, i++) {
+	    v0 = poly.vertices[j];
+	    v1 = poly.vertices[i];
+	    edge = v1.minus(v0);
+	    axis = edge.perp();
+	    if (this.separatedByAxis(axis, poly, info)) {
+		return info;
+	    }
+	}
+	info.overlapped = true;
+	return info;
+    };
+
+    // calculate the largest projection of the polygon on the given axis.
+    this.project = function(axis) {
+	var min, max, i, d;
+	min = max = this.vertices[0].dot(axis);
+	
+	for (i = 1; i < this.vertices.length; i++) {
+	    d = this.vertices[i].dot(axis);
+	    if (d < min) {
+		min = d;
+	    } else if (d > max) {
+		max = d;
+	    }
+	}
+	return new Interval(min, max);
+    };
+
+    this.separatedByAxis = function(axis, poly, info) {
+	var d0, d1;
+	var overlap;
+	var axisLenSquared;
+	var sep, sepLenSquared;
+	var proj1 = this.project(axis);
+	var proj2 = poly.project(axis);
+	
+	// calculate the two possible overlap ranges.
+	// either we overlap on the left or right of the polygon.
+	d0 = proj2.max - proj1.min;
+	d1 = proj2.min - proj1.max;
+
+	if (d0 < 0 || d1 > 0) {
+	    return true;
+	}
+	
+	// find out if we overlap on the 'right' or 'left' of the polygon.
+	overlap = (d0 < -d1)? d0 : d1;
+	
+	// the mtd vector for the axis
+	sep = axis.timesScalar(overlap / axis.dot(axis));
+	sepLenSquared = sep.dot(sep);
+
+	if (sepLenSquared < info.mtdLenSquared || info.mtdLenSquared < 0) {
+	    info.mtdLenSquared = sepLenSquared;
+	    info.mtd = sep;
+	}
+	return false;		
+    };
+}
+
+function CollisionInfo() {
+    this.mtdLenSquared = 0;
+    this.mtd = new Vector(0,0);
+    this.overlapped = false;    
+}
+
+// All sprites use rectangular polygons the same size as an image frame for now.
+// They are always oriented at a 0 deg angle on initialization.
+//
 function Sprite(name, numFrames, x, y) {
     var that = this;
     this.type = Sprite;
     this.ctx = appMgr.ctx;
     this.numFrames = numFrames;
-    this.x = x;
-    this.y = y;
-    this.rotationAngle = 0;
     this.img = new Image();
     this.img.src = name + ".png";
+    this.pos = new Vector(x, y); // Upper left corner of rectangle
+    this.vx = 0;
+    this.vy = 0;
+    this.accX = 1;
+    this.accY = 1;
+    this.angle = 0;
+    this.mass = 1;
+    this.width = 0;
+    this.height = 0;
+    this.polygon = null;
+
     this.img.onload = function() {
 	that.width = that.img.width/numFrames;
 	that.height = that.img.height;
-	that.topLeft = new Point(that.x, that.y);
-	that.bottomLeft = new Point(that.x, that.y + that.height);
-	that.topRight = new Point(that.x + that.width, that.y);
-	that.bottomRight = new Point(that.x + that.width, that.y + that.height);
-	that.center = new Point(that.x + that.width/2, that.y + that.height/2);
+	var vertices = [new Vector(that.pos.x, that.pos.y), 
+			new Vector(that.pos.x, that.pos.y + that.height),
+			new Vector(that.pos.x + that.width, that.pos.y), 
+			new Vector(that.pos.x + that.width, that.pos.y + that.height)];
+	that.polygon = new Polygon(vertices);
 	game.imgLoadCt++;
-    };
-
-    this.setPoints = function() {
-	this.topLeft.x = this.x;
-	this.topLeft.y = this.y;
-	this.bottomLeft.x = this.x;
-	this.bottomLeft.y = this.y + this.height;
-	this.topRight.x = this.x + this.width;
-	this.topRight.y = this.y;
-	this.bottomRight.x = this.x + this.width;
-	this.bottomRight.y = this.y + this.height;
-	this.center.x = this.x + this.width/2;
-	this.center.y = this.y + this.height/2;
-    };
-
-    this.rotate = function(angle) {
-	var sin = Math.sin(this.rotationAngle);
-	var cos = Math.cos(this.rotationAngle);
-	this.rotationAngle += angle;
-	this.topLeft.x = cos*this.x;
-	this.topLeft.y = sin*this.y;
-	this.bottomLeft.x = cos*this.x;
-	this.bottomLeft.y = sin*this.y+this.height;
-	this.topRight.x = cos*this.x + this.width;
-	this.topRight.y = sin*this.y;
-	this.bottomRight.x = cos*this.x + this.width;
-	this.bottomRight.y = sin*this.y+this.height;
     };
     
     this.draw = function() {
@@ -64,8 +216,8 @@ function Sprite(name, numFrames, x, y) {
 	var sHeight = this.height;
 	var dWidth = Math.floor(this.width);
 	var dHeight = this.height;
-	var dx = this.x;
-	var dy = this.y;
+	var dx = this.pos.x;
+	var dy = this.pos.y;
 
 	this.ctx.save();
 	if (this.rotationAngle) {
@@ -86,9 +238,9 @@ function Physics() {
 	for (i = 0; i < sprites.length; i++) {
 	    sprites[i].vx *= sprites[i].accX;
 	    sprites[i].vy *= sprites[i].accY;
-	    sprites[i].y += sprites[i].vy;
-	    sprites[i].x += sprites[i].vx;
-	    sprites[i].setPoints();
+	    sprites[i].pos.x += sprites[i].vx;
+	    sprites[i].pos.y += sprites[i].vy;
+	    sprites[i].polygon.translate(sprites[i].vx, sprites[i].vy);
 	}
     };
 
@@ -103,102 +255,14 @@ function Physics() {
 	s2.vy = s2vy;
     };
 
-    // Project Polygon (sprite) onto axis to determine min/max
-    var projectPoly = function(sprite, axis) {
-	var points = [sprite.topLeft, sprite.bottomLeft, sprite.topRight, sprite.bottomRight];
-	var min, max;
-	var dp, i;
-	min = max = points[0].x * axis.x + points[0].y * axis.y;
-	for (i = 1; i < points.length; i++) {
-	    dp = points[i].x * axis.x + points[i].y * axis.y;
-	    if (dp > max) { 
-		    max = dp;
-		} else if (dp < min) {
-		    min = dp;
-		}
+    this.collide = function(s1, s2) {
+	var collisionInfo = s1.polygon.intersect(s2.polygon);
+	var halfmtd = collisionInfo.mtd.timesScalar(.5);
+	if (collisionInfo.overlapped === true) {
+	    s1.polygon.plus(halfmtd);
+	    s2.polygon.minus(halfmtd);
+	    s1.pos = s1.pos.plus(halfmtd);
+	    s2.pos = s2.pos.minus(halfmtd);
 	}
-	
-	// Correct for center offset
-	dp = sprite.center.x * axis.x + sprite.center.y * axis.y;
-	min += dp;
-	max += dp;
-	return new Projection(min, max);
-    };   
-    
-    var projectionIntersect = function(points, s1, s2) {
-	var i, t;
-	var axis = new Point(0,0);
-	var proj1, proj2;
-	for (i = 0; i < points.length; i++) {
-	    // Figure out the axis to project onto
-	    if (i === 0) {
-		axis.x = points[points.length - 1].y - points[0].y;
-		axis.y = points[0].x - points[points.length - 1].x;
-	    } else {
-		axis.x = points[i - 1].y - points[i].y;
-		axis.y = points[i].x - points[i - 1].x;
-	    }
-	    
-	    // Normalize the axis
-	    t = Math.sqrt(axis.x*axis.x + axis.y*axis.y);
-	    axis.x /= t;
-	    axis.y /= t;
-	    
-	    proj1 = projectPoly(s1, axis);	    
-	    proj2 = projectPoly(s2, axis);
-	    
-	    /* test if lines intersect, if not, return false */
-	    if (proj1.max < proj2.min || proj1.min > proj2.max) {
-		return false; 
-	    }
-	}
-	return true;
-    }; 
-
-    // Use separating axis theorem with projection.
-    // http://en.wikipedia.org/wiki/Separating_axis_theorem
-    // http://gpwiki.org/index.php/VB:Tutorials:Building_A_Physics_Engine:Basic_Intersection_Detection
-    // http://gpwiki.org/index.php/IntersectionTestInC
-    // http://www.sevenson.com.au/actionscript/sat/
-    this.collision = function(s1, s2) {
-	var s1Points = [s1.topLeft, s1.bottomLeft, s1.topRight, s1.bottomRight];
-	var s2Points = [s2.topLeft, s2.bottomLeft, s2.topRight, s2.bottomRight];
-
-	if (projectionIntersect(s1Points, s1, s2) && projectionIntersect(s2Points, s1, s2)) {
-	    return true;
-	} else {
-	    return false;
-	}
-    };
-
-    this.wallCollideAndBounce = function(sprites, canvasWidth, canvasHeight) {
-    	var i = null;
-    	var colliders = new Array();
-    	var sprite = null;
-    	for (i = 0; i < sprites.length; i++) {
-    	    sprite = sprites[i];
-    	    if (sprite.vx < 0 && (sprite.topLeft.x <= 0 || sprite.topRight.x <= 0 || 
-				  sprite.bottomLeft.x <= 0 || sprite.bottomRight.x <= 0)) {
-    		sprite.vx = -sprite.vx;
-    		colliders.push(sprite);
-	    } else if (sprite.vx > 0 && (sprite.topLeft.x >= canvasWidth || 
-					 sprite.topRight.x >= canvasWidth || 
-					 sprite.bottomLeft.x >= canvasWidth ||
-					 sprite.bottomRight.x >= canvasWidth)) {
-		sprite.vx = -sprite.vx;
-		colliders.push(sprite);
-	    } else if (sprite.vy < 0 && (sprite.topLeft.y <= 0 || sprite.topRight.y <= 0 ||
-					 sprite.bottomLeft.y <= 0 || sprite.bottomRight.y <= 0)) {
-		sprite.vy = -sprite.vy;
-		colliders.push(sprite);
-	    } else if (sprite.vy > 0 && (sprite.topLeft.y >= canvasHeight || 
-					 sprite.topRight.y >= canvasHeight || 
-					 sprite.bottomLeft.y >= canvasHeight ||
-					 sprite.bottomRight.y >= canvasHeight)) {
-		sprite.vy = -sprite.vy;
-		colliders.push(sprite);
-	    }
-	}
-	return colliders;
     };
 }
--- dice.js
+++ dice.js
@@ -15,29 +15,19 @@ function DiceGame() {
     this.bg.onload = function() {
 	game.imgLoadCt++;
     };
-    this.dice1 = new Sprite("dice-all", 6, 0, 0);
-    this.dice2 = new Sprite("dice-all", 6, 0, 0);
+    this.dice1 = new Sprite("dice-all", 6, appMgr.width/2, appMgr.height/2);
+    this.dice2 = new Sprite("dice-all", 6, appMgr.width/2 + 50, appMgr.height-50);
     this.diceBounce = 1;
     this.drawTimer = null;
     this.physics = new Physics();
     this.state = 'init';
     
     this.initDice = function() {
-	this.dice1.x = appMgr.width/2;
-	this.dice1.y = appMgr.height/2;
 	this.dice1.vx = 10;
 	this.dice1.vy = 5;
-	this.dice1.accX = 1;
-	this.dice1.accY = 1;
-	this.dice1.mass = 1;
 
-	this.dice2.x = appMgr.width/2 + this.dice1.width;
-	this.dice2.y = appMgr.height/2;
-	this.dice2.vx = -20;
-	this.dice2.vy = 2;
-	this.dice2.accX = 1;
-	this.dice2.accY = 1;
-	this.dice2.mass = 1;
+	this.dice2.vx = -2;
+	this.dice2.vy = -20;
     };
 
     this.play = function() {
@@ -74,11 +64,11 @@ function DiceGame() {
 	    dice1.frameIndex = Math.floor(Math.random() * dice1.numFrames);
 	    dice2.frameIndex = Math.floor(Math.random() * dice2.numFrames);
 	    game.physics.move([dice1, dice2]);
-	    if (game.physics.collision(dice1, dice2)) {
+	    if (game.physics.collide(dice1, dice2)) {
 		game.physics.bounce(dice1, dice2, game.diceBounce);
 	    }
-	    var colliders = game.physics.wallCollideAndBounce([dice1, dice2], 
-							      appMgr.width, appMgr.height);
+	    //	    var colliders = game.physics.wallCollideAndBounce([dice1, dice2], 
+	    //						      appMgr.width, appMgr.height);
 	    //dice1.rotate(.3);
 	    dice1.draw();
 	    //dice2.rotate(.3);
