6f1f5bb3492d5b386210a5904f0ba69074296b72
Andrew Stone <astone@li168-45.members.linode.com>
2011-02-10 20:22:08 +0000

    install mongo from apt repo

--- install/install.sh
+++ install/install.sh
@@ -11,10 +11,3 @@ nvm use v0.2.6
 
 # node packages
 npm install connect-auth express jade mongodb mongoose monomi oauth qs socket.io supervisor
-
-# mongo-db
-cd ~
-MONGO_VER=mongodb-linux-x86_64-1.6.5
-wget http://fastdl.mongodb.org/linux/${MONGO_VER}.tgz
-tar xzf ${MONGO_VER}.tgz
-rm ${MONGO_VER}.tgz
--- install/install_privileged.sh
+++ install/install_privileged.sh
@@ -1,14 +1,16 @@
+# Add mongodb packages to apt repository
+echo "deb http://downloads.mongodb.org/distros/ubuntu 10.10 10gen" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+
 # bring the base ubuntu distribution up to date
 apt-get update
 apt-get upgrade
 
+# install mongodb
+apt-get install mongodb-stable
+
 # install essential libraries and build tools
 apt-get install -y build-essential libssl-dev
-
-# create mongodb directories
-mkdir -p /data/db/
-chown `id -u` /data/db
-# FIXME: cp a run script to the right spot to be started on boot
 
 # install useful tools and utilities
 apt-get install -y emacs vim
