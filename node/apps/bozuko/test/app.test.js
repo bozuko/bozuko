ee6238bb7005f116e5529401c52954fb22ca4020
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-02-16 06:29:59 +0000

    add a passing test. It only passes when ^C is pressed. It is hanging for an unknown reason.

--- node/apps/bozuko/test/app.test.js
+++ node/apps/bozuko/test/app.test.js
@@ -5,15 +5,19 @@
  * Module dependencies.
  */
 
+var assert = require('assert');
 var app = require('../app');
 
 module.exports = {
-  'GET /': function(assert){
-    assert.response(app,
-      { url: '/' },
+  'GET /': function(beforeExit) {
+      assert.response(app, { url: '/' },
       { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
       function(res){
-          assert.includes(res.body, '<title>Express</title>');
+          assert.includes(res.body, '<title>Bozuko</title>');
+      });
+      
+      beforeExit(function() {
+          Bozuko.db.conn().disconnect();
       });
   }
 };
