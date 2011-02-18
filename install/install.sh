83e357af3175fcc30258a6e4fd667cb2db8f1f04
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-02-18 07:53:45 +0000

    fix install.sh so it doesn't need root.

--- install/install.sh
+++ install/install.sh
@@ -7,18 +7,44 @@ LIB=~/bozuko/node/lib
 mkdir -p $LIB
 
 # node version manager (nvm)
+echo "*** Installing nvm"
 git clone git://github.com/creationix/nvm.git ~/.nvm
 echo '. ~/.nvm/nvm.sh' >> ~/.bashrc
 . ~/.nvm/nvm.sh
 
+# Create a fake npm so that nvm doesn't try to install it
+# this is needed to prevent installing with sudo
+echo "*** Installing fake npm"
+PATH=~/bozuko/install:$PATH
+touch npm
+chmod +x npm
+
 # node.js
+echo "*** Installing node $NODE_VER"
 nvm install $NODE_VER
+echo "*** Using node $NODE_VER"
 nvm use $NODE_VER
 
+# Remove fake npm
+echo "*** Removing fake npm"
+rm npm
+
+#node package manager (npm)
+echo "*** Installing npm"
+git clone http://github.com/isaacs/npm.git
+cd npm
+git submodule update --init
+npm_config_unsafe_perm=true make install
+cd ..
+rm -rf npm
+
 # node packages
+echo "*** Installing node packages with npm"
+npm config set unsafe-perm true
 npm install connect-auth express jade mongodb mongoose monomi oauth qs socket.io supervisor
 
 # multi-node
+echo "*** Installing multi-node"
 git clone https://github.com/kriszyp/multi-node.git multi-node
 mv multi-node/lib/multi-node.js $LIB
 rm -rf multi-node
