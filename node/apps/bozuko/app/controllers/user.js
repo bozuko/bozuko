098f74d6242dfbb409be58c980f962801c4b05c0
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-03-03 04:26:44 +0000

    fill out user controller and test more

--- node/apps/bozuko/app/controllers/user.js
+++ node/apps/bozuko/app/controllers/user.js
@@ -5,32 +5,72 @@ var facebook    = bozuko.require('util/facebook'),
     qs          = require('querystring')
 ;
 
-var fakePrizes = {
-	    active : [{name: 'buffalo wings'}],
-	    redeemed : [{name: 'potato skins'}],
-            expired : [{name: 'nachos'}]	
-        };
-
 exports.routes = {
-    
+
     '/user/login' : {
-        
-        description :"User login - sends user to facebook",
-        
+
+	description :"User login - sends user to facebook",
+
         aliases     :['/login'],
-        
+
         get : function(req,res){
             bozuko.require('auth').login(req,res,'user');
         }
     },
 
-   '/user/:id/prizes' : {
-	
+    '/user/:id' : {
+
+	description : "Return user properties",
+
+	get : function(req, res) {
+	    res.send({
+		id: req.params.id,
+		name: 'bozukob',
+		first_name: 'bobby',
+		last_name: 'bozuko',
+		gender: 'm',
+		email: 'bozukob@gmail.com',
+		picture: 'http://someImageUrl',
+		facebook_id: 2323423,
+		can_manage_pages: 'true'
+	    });
+	}
+    },
+
+    '/user/:id/prizes' : {
+
 	description : "Return the user's prizes",
 
 	get : function(req, res) {
-	   res.send(fakePrizes)	   	
+	    active_prize = {
+		state: 'active',
+		name: 'wings',
+		place: 'hookslides',
+		win_time: new Date().toString(),
+		expiration_time: new Date(2012, 'july', 4)
+	    };
+	    redeemed_prize = {
+		state: 'redeemed',
+		name: 'wings',
+		place: 'hookslides',
+		win_time: new Date().toString(),
+		redemption_time: new Date(2011, 'july', 4)
+	    };
+	    expired_prize = {
+		state: 'expired',
+		name: 'wings',
+		place: 'hookslides',
+		win_time: new Date().toString(),
+		expiration_time: new Date(2011, 'Feb', 28)
+	    };
+
+	    /* Do we want to send just one list and tag each prize with a state? */
+	    res.send({
+		active: [active_prize],
+		redeemed: [redeemed_prize],
+		expired: [expired_prize]
+	    });
         }
-   }		
+    }
 
 };
--- node/apps/bozuko/test/user.test.js
+++ node/apps/bozuko/test/user.test.js
@@ -2,10 +2,46 @@ var print = require('util').debug;
 var assert = require('assert');
 var bozuko = require('bozuko');
 
+var fake_id = 'fake-id';
+
+assert.validate = function(object, properties) {
+    properties.forEach(function(prop) {
+	assert.ok(prop in object);
+    });
+};
+
 exports['GET /user/login'] = function(beforeExit) {
     assert.response(bozuko.app,
         {url: '/user/login'},
         {status: 302, headers: {'Content-Type': 'text/html'}},
         function(res) {
+	});
+};
+
+exports['GET /user/:id'] = function(beforeExit) {
+    assert.response(bozuko.app,
+	{url: '/user/'+fake_id},
+	{status: 200, headers: {'Content-Type': 'application/json'}},
+	function(res) {
+	    var user = JSON.parse(res.body);
+	    assert.eql(fake_id, user.id);
+	    assert.validate(user, ['name', 'first_name', 'last_name', 'gender', 'email', 'picture',
+	        'facebook_id', 'can_manage_pages']);
+	});
+};
+
+exports['GET /user/:id/prizes'] = function(beforeExit) {
+    assert.response(bozuko.app,
+	{url: '/user/'+fake_id+'/prizes'},
+	{status: 200, headers: {'Content-Type': 'application/json'}},
+	function(res) {
+	    var prizes = JSON.parse(res.body);
+	    assert.validate(prizes, ['active', 'redeemed', 'expired']);
+	    assert.eql(prizes.active[0].state, 'active');
+	    assert.validate(prizes.active[0], ['name', 'place', 'win_time', 'expiration_time']);
+	    assert.eql(prizes.redeemed[0].state, 'redeemed');
+	    assert.validate(prizes.redeemed[0], ['name', 'place', 'win_time', 'redemption_time']);
+	    assert.eql(prizes.expired[0].state, 'expired');
+	    assert.validate(prizes.expired[0], ['name', 'place', 'win_time', 'expiration_time']);
 	});
 };
