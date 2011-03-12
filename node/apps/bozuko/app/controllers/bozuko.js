84c96c4cdce04363f3a858cbe38a65dd32cd98ba
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-03-12 09:58:01 +0000

    add bozuko_accept_terms link

--- node/apps/bozuko/app/controllers/bozuko.js
+++ node/apps/bozuko/app/controllers/bozuko.js
@@ -5,8 +5,14 @@ exports.object_types = {
         rules: "String",
         about: "String",
         creating_contest: "String",
-        terms: "String",
+        terms: {
+            text: "String",
+            accepted: "Boolean",
+            timestamp: "Date"
+        },
         links: {
+            user: "String",
+            bozuko_accept_terms: "String",
             contest: "String",
             contest_result: "String"
         }
@@ -18,6 +24,18 @@ exports.links = {
         get: {
             description: "Retrieve information about bozuko",
             returns: "bozuko"
+        }
+    },
+    bozuko_accept_terms: {
+        put: {
+            description: "Accept the Bozuko terms of use",
+            params: {
+                accept: {
+                    required: true,
+                    type: "Boolean",
+                    description: "Does the user accept the the terms of use"
+                }
+            }
         }
     }
 };
