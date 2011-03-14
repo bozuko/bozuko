a462a5722757c746015c030a7eab330ba495412f
Mark Fabrizio <fabrizim@owlwatch.com>
2011-03-14 00:45:13 +0000

    Sanitize function

--- node/apps/bozuko/app/core/transfer.js
+++ node/apps/bozuko/app/core/transfer.js
@@ -16,48 +16,48 @@ $.sanitize = function(data, current){
     // make this conform to our def
     var self = this, ret = {};
     if( !current ) current = this.def;
-    Object.keys( current ).forEach( function( key ){
-        if( data[key] ){
-            // make sure its the right type
-            var v = ret[key] = data[key];
-            var c = current[key];
-            if( c instanceof String ){
-                // check type
-                switch(c.toLowerCase()){
-                    
-                    case 'string':
-                        v = ''+v;
-                        break;
-                    
-                    case 'int':
-                    case 'integer':
-                        v = parseInt(v);
-                        break;
-                    
-                    case 'float':
-                    case 'number':
-                        v = parseFloat(v);
-                        break;
-                }
-            }
-            else if(c instanceof Array){
-                if( !(v instanceof Array) ){
-                    v = [v];
-                }
-                // the configuration should only have one value
-                v.forEach(function(obj){
-                    obj = self.sanitize(obj,c[0]);
-                });
-            }
-            else if(c instanceof Object){
-                v = self.sanitize(v, c);
-            }
+    
+    if( current instanceof Array ){
+        if( !(data instanceof Array) ){
+            ret = [];
         }
-    });
+        data.forEach(function(v,k){
+            data[k] = self.sanitize(v,current[0]);
+        });
+    }
+    else if( current instanceof Object ){
+        Object.keys(current).forEach(function(key){
+             if( data[key] ){
+                // Cast the value to the proper type.
+                var v = ret[key] = data[key];
+                var c = current[key];
+                if( c instanceof String ){
+                    // check type
+                    switch(c.toLowerCase()){
+                        
+                        case 'string':
+                            v = ''+v;
+                            break;
+                        
+                        case 'int':
+                        case 'integer':
+                            v = parseInt(v);
+                            break;
+                        
+                        case 'float':
+                        case 'number':
+                            v = parseFloat(v);
+                            break;
+                    }
+                }
+                else if(c instanceof Object){
+                    ret[key] = self.sanitize(v,c);
+                }
+            }
+        });
+    }
     return ret;
 };
-
-
 
 $.addLink = function(link){
     this.links[link.name] = link;
