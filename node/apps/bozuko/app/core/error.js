99f74c71a340dc86c713a3e8ba04fc7d54a4b633
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-03-21 17:59:36 +0000

    remove old stuff

--- node/apps/bozuko/app/core/error.js
+++
@@ -1,8 +0,0 @@
-// meh, don't know if we need this - just use vanilla Error
-
-var BozukoError = function(message, data, file, line){
-    this.data = data;
-    Error.call(this, message, file, line);
-};
-
-BozukoError.prototype.__proto__ = Error.prototype;
--- node/apps/bozuko/docs/api/links.md
+++
@@ -1,3 +0,0 @@
-### Links
-
-Describe Links
