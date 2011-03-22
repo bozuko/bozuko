13b28ba0c4c4b00392bbfd61827fece324d468f1
Mark Fabrizio <fabrizim@owlwatch.com>
2011-03-22 04:43:57 +0000

    Reduced the Error code down to the global Error Object

--- app/core/error.js
+++ app/core/error.js
@@ -4,7 +4,6 @@ var bozuko          = require('bozuko'),
 var BozukoError = module.exports = function(name,message,data,code){
     Error.call(this);
     Error.captureStackTrace(this, arguments.callee);
-    this.name = 'default';
     this.data = data;
     if( code ) this.code = code;
     if( message instanceof Function)
@@ -14,10 +13,13 @@ var BozukoError = module.exports = function(name,message,data,code){
 };
 
 var proto = BozukoError.prototype.__proto__ = Error.prototype;
-MongooseError.prototype.code = 500;
-proto.code = 500;
 
-proto.toTransfer = function(){
+Error.prototype.code = 500;
+Error.prototype.name = 'default';
+
+MongooseError.prototype.name='mongoose';
+
+Error.prototype.toTransfer = function(){
     return bozuko.transfer('error', this);
 };
 
@@ -25,11 +27,6 @@ proto.generateMessage = function(fn){
     this.message = fn.apply(this);
 };
 
-MongooseError.protype.send = proto.send = function(res){
+Error.prototype.send = function(res){
     return res.send( this.toTransfer(), this.code );
-};
-
-MongooseError.prototype.toTransfer = function(){
-    this.name = 'mongoose';
-    return proto.toTransfer.apply(this);
 };
