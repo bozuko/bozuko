41e48e69ec611170f0af6846328d22edcc00ff18
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-03-30 21:36:02 -0400

    remove test/stats.js from git

--- test/stats.js
+++
@@ -1,9 +0,0 @@
-var stats = Bozuko.require('util/stats');
-var assert = require('assert');
-
-exports['stats collect'] = function() {
-    stats.collect_all(function(err, val){
-        assert.isNull(err);
-        assert.eql(val, 'ok');
-    });
-};
