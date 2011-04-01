ee1299675d247681df981513d2f6076e8472f036
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-04-01 00:42:56 -0400

    add scratch ticket and tests

---
+++ app/games/test/scratch.test.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+Bozuko = require('../../../bozuko.js');
+
+// Mock contest
+var contest = {
+    game_config: {}
+};
+
+var Scratch = require('../scratch/index.js');
+var scratch = new Scratch(contest);
+
+exports['lose'] = function() {
+    var result = scratch.process(false);
+    result.user_numbers.forEach(function(num) {
+        for (var i = 0; i < result.winning_numbers.length; i++) {
+            assert.ok(result.winning_numbers[i] != num);
+        }
+    });
+};
+
+exports['win'] = function() {
+    var match_index = 2;
+    var result = scratch.process(match_index);
+    var num_matches = 0;
+    result.user_numbers.forEach(function(num) {
+        for (var i = 0; i < result.winning_numbers.length; i++) {
+            if (match_index === i) {
+                if (result.winning_numbers[match_index] === num) {
+                    num_matches++;
+                }
+            } else {
+                assert.ok(result.winning_numbers[i] != num);
+            }
+        }
+    });
+    assert.eql(num_matches, 1);
+};
