d070fab0a37cff2953ad3ca1a9be9880f5688d75
Mark Fabrizio <fabrizim@owlwatch.com>
2011-04-22 13:13:41 -0400

    Small doc change to add icons to auth methods

--- app/static/js/desktop/business/sign-up.js
+++ app/static/js/desktop/business/sign-up.js
@@ -20,8 +20,27 @@ Ext.onReady(function(){
             // new x, y
             ax = cx - auth_w/2,
             ay = cy - auth_h/2;
-            
+        
         auth_win = window.open(url+'&display=popup','auth_window','width='+auth_w+',height='+auth_h+',left='+ax+',top='+ay);
         return true;
     });
+    
+    // create the window method for sucessfull login
+    window.onLoginSuccess = function(){
+        if( auth_win && !auth_win.closed ) setTimeout(function(){
+            auth_win.close();
+            window.focus();
+            // go to the next page
+            window.location = '/business/sign-up/account'
+        }, 1000);
+    };
+    // create the window method for sucessfull login
+    window.onLoginFailure = function(){
+        if( auth_win && !auth_win.closed ) setTimeout(function(){
+            auth_win.close();
+            window.focus();
+            // go to the next page
+            window.location = '/business/sign-up/error'
+        }, 1000);
+    };
 });
