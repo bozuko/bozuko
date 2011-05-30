06dc4621cfa874ec7d36c58fa072c736ea36e953
Mark Fabrizio <fabrizim@owlwatch.com>
2011-05-30 10:47:25 -0400

    Removed debug

--- tests/system/nonplace.test.js
+++ tests/system/nonplace.test.js
@@ -36,7 +36,6 @@ exports.get_pages = function(test) {
         {url: pages_link+'/?ll='+ll+'&limit=2'},
         ok,
         function(res) {
-            console.log(res.body);
             var result = JSON.parse(res.body);
             var page = result.pages[0];
             console.log(page.location);
