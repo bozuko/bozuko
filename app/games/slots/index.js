281602f9fa615437ff9b6d2f626613b7ca50f7c5
Mark Fabrizio <fabrizim@owlwatch.com>
2011-05-31 12:15:27 -0400

    Updated slot result icon config

--- app/games/slots/index.js
+++ app/games/slots/index.js
@@ -111,7 +111,7 @@ Slots.prototype.createResultImage = function(dest, icon_src){
             gd.openPng(
                 icon_src,
                 function(icon, path){
-                    icon.copyResampled(x3,5,15,0,0,50,50,icon.width,icon.height);
+                    icon.copyResampled(x3,0,15,0,0,50,50,icon.width,icon.height);
                     x3.saveAlpha(1);
                     x3.savePng(dest, 0, gd.noop);
                 }
