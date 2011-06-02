de30d383f6e2f52755362c3e5d0bdbe3c7401bee
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-02 11:23:06 -0400

    Updated install.sh with knox, fixed the logic for version comparison with trailing digits after alpha,beta,rc

--- install/install.sh
+++ install/install.sh
@@ -16,4 +16,4 @@ source ~/.bashrc
 
 # node packages
 echo "*** Installing node packages with npm"
-cd ~ && npm install connect connect-auth express jade mongodb mongoose monomi oauth qs socket.io markdown-js async sprintf nodeunit nodemailer dateformat relative-date cluster imap node-uuid node-gd
+cd ~ && npm install connect connect-auth express jade mongodb mongoose monomi oauth qs socket.io markdown-js async sprintf nodeunit nodemailer dateformat relative-date cluster imap node-uuid node-gd knox
