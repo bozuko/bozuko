cef7d63165d123f8870a831a981c0e08d0d409a8
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-03 17:47:42 -0400

    Added correct scratch background

--- app/static/js/desktop/admin/view/admin/Dashboard.js
+++ app/static/js/desktop/admin/view/admin/Dashboard.js
@@ -3,10 +3,44 @@ Ext.define('Bozuko.view.admin.Dashboard' ,{
     extend: 'Ext.panel.Panel',
     alias : 'widget.admindashboard',
     
+    bodyPadding: 10,
+    title:"Admin Dashboard",
+    /*
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+    */
     initComponent : function(){
-        this.title = "Admin Dashboard";
+        
         this.html = 'The admin dashboard';
-        this.bodyPadding = 10;
+        
+        /*
+         // okay.. lets add a chart.
+        this.items = [{
+            flex: 1,
+            layout: {
+                type: 'hbox',
+                layout: 'stretch'
+            },
+            items: [{
+                border: false,
+                html: 'pretty big'
+            },{
+                margin: 4,
+                frame: true,
+                title: 'System',
+                layout: 'fit',
+                items: [{
+                    xtype: 'profiler'
+                }]
+            }]
+        },{
+            flex: 2,
+            border: false,
+            html: 'big chart'
+        }];
+        */
         this.callParent();
     }
 });
