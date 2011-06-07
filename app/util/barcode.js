ed0674355f5be07ca8d9d85fc930c5c26456ff4e
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-06-07 18:45:58 -0400

    allow proper barcode sizing

--- app/util/barcode.js
+++ app/util/barcode.js
@@ -1,18 +1,18 @@
 var exec = require('child_process').exec;
 
 exports.create_png = function(barcode, encoding, filename, callback) {
-    exec('barcode -b '+barcode+' -e '+encoding+' -o '+filename+'.ps'+' -u pt'+' -g 100x40'+' -p 100x40', function(err, stdout, stderr) {
+    exec('barcode -b '+barcode+' -e '+encoding+' -o '+filename+'.ps'+' -u pt'+' -g 100x40 -E', function(err, stdout, stderr) {
+        if (stderr.length != 0) console.error(stderr);
         if (err) {
             console.error(err);
-            console.error(stderr);
             return callback(err);
         }
 
         exec('convert -density 100 '+filename+'.ps'+' -flatten '+ filename+'.png',
             function(err, stdout, stderr) {
+                if (stderr.length != 0) console.error(stderr);
                 if (err) {
                     console.error(err);
-                    console.error(stderr);
                     return callback(err);
                 }
                 return callback(null);
