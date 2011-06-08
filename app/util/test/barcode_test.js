811951f01f5eb593ae060f6cf32cfecf54272343
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-08 14:08:31 -0400

    Removed the png and ps and fixed the barcode test

--- app/util/test/barcode_test.js
+++ app/util/test/barcode_test.js
@@ -5,15 +5,20 @@ var barcode = require('../barcode'),
 
 // setup the tmp folder
 var filename = __dirname+'/barcode_test';
-
-barcode.create_png('123456789012', 'upc', filename, function(err) {
-    assert.ok(!err);
-    
-    /**
-     * Cleanup
-     */
-    ['.ps','.png'].forEach(function(ext){
-        if( path.existsSync(filename+ext) ) fs.unlinkSync( filename+ext);
+module.exports['Test Barcode Creation'] = function(test){
+    barcode.create_png('123456789012', 'upc', filename, function(err) {
+        
+        test.ok(!err);
+        test.done();
+        
+        /**
+         * Cleanup
+         */
+        ['.ps','.png'].forEach(function(ext){
+            if( path.existsSync(filename+ext) ){
+                fs.unlinkSync( filename+ext);
+            }
+        });
+        
     });
-    
-});
+}
