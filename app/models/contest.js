964fb5da76cc4b5d560c2e0f801c94b4a8a8835e
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-06-12 03:09:59 -0400

    only end game if user is out of tokens

--- app/models/contest.js
+++ app/models/contest.js
@@ -307,10 +307,11 @@ Contest.method('loadGameState', function(user, callback){
     }
 
     // Contest is over for this user
-    if (this.token_cursor == this.total_plays - this.total_free_plays) {
+    if (state.user_tokens === 0 && this.token_cursor == this.total_plays - this.total_free_plays) {
+	console.error("GAME OVER");
         state.game_over = true;
+	state.next_enter_time = 'Never';
         state.button_enabled = false;
-        state.next_enter_time = 'Never';
         return callback(null, state);
     }
 
