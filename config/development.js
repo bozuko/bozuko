d733b1421136b2a675ec404db3ad2b7de5b492f0
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-17 00:06:54 -0400

    Trying to get stupid featured to work

--- config/development.js
+++ config/development.js
@@ -88,7 +88,7 @@ module.exports = {
         // radius to search for "Nearby Games" in miles
         nearbyRadius: 2,
         nearbyLimit: 10,
-        featuredResults: 1
+        featuredResults: 2
     },
 
     server: {
