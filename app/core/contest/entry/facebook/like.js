48261eadb703ba790d15eec0a10d3ecfa972a548
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-17 10:53:04 -0400

    Updated like description

--- app/core/contest/entry/facebook/like.js
+++ app/core/contest/entry/facebook/like.js
@@ -87,9 +87,9 @@ FacebookLikeMethod.prototype.getDescription = function(){
     var description = "Like us on Facebook\n";
         description+= this.config.tokens+" "+(this.config.tokens > 1 ? "Plays" : "Play" )+" every "+duration;
         
-        //if( !self.user || !self.user_likes ){
+        if( !self.user || !self.user_likes ){
             description+="\n\nHit back and scroll down to like us."
-        //}
+        }
     
     return description;
 }
