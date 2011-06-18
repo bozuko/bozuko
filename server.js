ebe1ff7edf72eabebbacdcd8ff018fb299ec888d
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-18 10:22:57 -0400

    Simplified server.js. Made config so you can turn controllers on and off with "only" or "except" options. Moved sockets into their own environment specific directory.

--- server.js
+++ server.js
@@ -1,75 +1,44 @@
-var cluster         = require('cluster');
+var cluster         = require('cluster'),
+    fs              = require('fs'),
+    path            = require('path')
+    ;
+
 Bozuko          = require('./app/bozuko');
 
 // var cfg = require('./config/'+(process.env.NODE_EVN || 'development'));
-var cfg = Bozuko.getConfig();
+var cfg = Bozuko.getConfig(), env = Bozuko.env();
+// create any neccessary directories
+var dirs = {
+    'logs':'*.log',
+    'pids':'*.pid',
+    'sockets':'*.sock'
+};
+
+Object.keys(dirs).forEach( function(name){
+    var dir = __dirname+'/'+name+'/'+env;
+    if( path.existsSync(dir) ) return;
+    fs.mkdirSync( dir, 0755 );
+    fs.writeFileSync(dir+'/.gitignore', dirs[name], 'utf8');
+});
 
 var proc = cluster( './app' )
-    .set('socket path', './sockets')
-
-    .in('development')
-        .use(cluster.logger('logs/development', 'debug'))
-        .use(cluster.debug())
-        .use(cluster.pidfiles('pids/development'))
-        .use(cluster.cli())
-        .use(cluster.stats())
-        .use(cluster.repl( cfg.server.port+10, '127.0.0.1' ) )
-        //.use(cluster.reload(['app','config','content'], {sig: 'SIGTERM'}))
-        .listen( cfg.server.port )
-        
-    .in('fuzz')
-        .use(cluster.logger('logs/fuzz', 'debug'))
-        .use(cluster.debug())
-        .use(cluster.pidfiles('pids/fuzz'))
-        .use(cluster.cli())
-        .use(cluster.stats())
-        .use(cluster.repl( cfg.server.port+10, '127.0.0.1' ) )
-        //.use(cluster.reload(['app','config','content'], {sig: 'SIGTERM'}))
-        .listen( cfg.server.port )
-
-    .in('stats')
-        .use(cluster.logger('logs/stats', 'debug'))
-        .use(cluster.debug())
-        .use(cluster.pidfiles('pids/stats'))
-        .use(cluster.cli())
-        .use(cluster.stats())
-        .use(cluster.repl( cfg.server.port+10, '127.0.0.1' ) )
-        .use(cluster.reload(['app','config','content']))
-        .listen( cfg.server.port )
-
-    .in('production')
-        .use(cluster.logger('logs/production', 'info'))
-        .use(cluster.pidfiles('pids/production'))
-        .use(cluster.cli())
-        .use(cluster.stats())
-        .use(cluster.repl( cfg.server.port+10, '127.0.0.1' ) )
-        .listen( cfg.server.port )
-    
-    .in('playground')
-        .use(cluster.logger('logs/playground', 'info'))
-        .use(cluster.pidfiles('pids/playground'))
-        .use(cluster.cli())
-        .use(cluster.stats())
-        .use(cluster.repl( '8069', '127.0.0.1' ) )
-        .listen( cfg.server.port )
-
-    .in('load')
-        .use(cluster.reload())
-        .use(cluster.logger('logs/load', 'info'))
-        .use(cluster.pidfiles('pids/load'))
-        .use(cluster.cli())
-        .use(cluster.stats())
-        .use(cluster.repl( cfg.server.port+10, '127.0.0.1' ) )
-        .listen( cfg.server.port );
+    .set( 'socket path', './sockets/'+env )
+    .use( cluster.logger('logs/'+env, cfg.logLevel || 'debug') )
+    .use( cluster.pidfiles('pids/'+env) )
+    .use( cluster.cli() )
+    .use( cluster.stats() )
+    .use( cluster.repl(cfg.server.port+10, '127.0.0.1') )
+    .listen( cfg.server.port )
+    ;
 
 
 if( proc.isMaster ){
 
-    if(Bozuko.env() === 'stats'){
+    if( env === 'stats'){
         Bozuko.initStats();
     }
 
-    if( Bozuko.env() === 'development' ){
+    if( env === 'development' ){
         // console.log('intializing development environment');
         // Bozuko.require('dev/setup').init();
     }
