12b98c65930ec9e1764b8a339e308f832ef8586b
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-22 14:10:47 -0400

    Added an api config - this will be the real deal production config

---
+++ config/api.js
@@ -0,0 +1,137 @@
+module.exports = {
+
+    test_mode: false,
+    
+    controllers: {
+        except: [
+            'site',
+            'business'
+        ]
+    },
+    
+    client: {
+        mobile:{
+            iphone:{
+                min_version: '1.0'
+            },
+            android:{
+                min_version: '1.0'
+            }
+        }
+    },
+    
+    admin : {
+        winners_list:{
+            poll_interval: 1000
+        }
+    },
+
+    db:{
+        name: 'bozuko',
+        host: 'localhost'
+    },
+
+    facebook: {
+        app:{
+            id:'166078836756369',
+            secret:'df03acad0cda5e6f2ec2161c00cf5bf3',
+            access_token:'166078836756369|5PhifMaZ8cZzgdlY4ZhfFPvGtOk',
+            pubsub_verify:'12kg0a8b9123kab91831laa9sb'
+        },
+        perms:{
+            user:"email,publish_checkins,publish_stream,offline_access,user_likes",
+            business:"email,manage_pages"
+        }
+    },
+    
+    bozuko :{
+        facebook_id: '223851717629478'
+    },
+
+    foursquare: {
+        app:{
+            id:'E43N3RJPOS2ULW0KTUSICZRFXB21VJWH55WEXTGMJPOQLL2K',
+            secret:'GXYMXF3HPXTNAQSBRJLAZVOGC25SKX4MBKYW0OQ40GW5IGYJ'
+        }
+    },
+    
+    amazon : {
+        s3:{
+            key:'AKIAJD7BVQJST2HCCPGA',
+            secret: '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',
+            bucket: 'bozuko'
+        }
+    },
+
+    checkin: {
+        duration: {
+            // 15 minutes between any checkins per user
+            user: 1000 * 60 * 15,
+            // 4 hours between checkins at the same place
+            page: 1000 * 60 * 60 * 4
+        },
+        
+        distance : 400 /* feet */
+    },
+
+    entry : {
+        token_expiration: 1000 * 60 * 60 * 24 * 1 // one day.
+    },
+
+    contest : {
+        engine : {
+            order: {
+                chunk_size: 2
+            }
+        }
+    },
+
+    search: {
+        // radius to search for "Nearby Games" in miles
+        nearbyRadius: 2,
+        nearbyLimit: 10,
+        featuredResults: 1
+    },
+
+    server: {
+        auth: false,
+        ssl: true,
+        host: 'api.bozuko.com',
+        port: 443
+    },
+    
+    pubsub: {
+        poll: {
+            interval: 500
+        },
+        cleanup: {
+            threshold: 1000 * 60 * 60 * 2,
+            interval: 1000 * 60 * 10
+        }
+    },
+
+    email : {
+        smtp:{
+            host: "smtp.gmail.com",
+            port: 465,
+            ssl: true,
+            use_authentication: true,
+            user: "mailer@bozuko.com",
+            pass: "7axxn7d8"
+        },
+        sender: 'Bozuko Mailer <mailer@bozuko.com>'
+    },
+
+    defaultService:'facebook',
+
+    key: '7ndfde983xg&*4dp11Lcdcrn111+@adf',
+
+    /**
+     * These are basic authenticated users...
+     */
+    auth: {
+        'bozuko'        :'cracksmack',
+        'developer'     :'bluebeard',
+        'guest'         :'virgil'
+    }
+};
