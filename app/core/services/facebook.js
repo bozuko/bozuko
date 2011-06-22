fcdd9e3955b3e45af290e8b241af8a977437d2ed
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-22 16:52:12 -0400

    Added post to service api

--- app/core/services/facebook.js
+++ app/core/services/facebook.js
@@ -382,6 +382,51 @@ FacebookService.prototype.like = function(options, callback){
 };
 
 /**
+ * Post to feed
+ *
+ * The callback will be passed 2 arguments
+ *
+ *      error
+ *      data
+ *
+ * @param {Object}          options         Post specific options
+ * @param {Function}        callback        Callback Function
+ *
+ * @option {User}           user            The user (required)
+ * @option {String}         message         The message for the wall post
+ * @option {Number}         place_id        The service specific id
+ *
+ * @return {null}
+ */
+FacebookService.prototype.post = function(options, callback){
+    if( !options || !options.object_id || !options.user ){
+        return callback(Bozuko.error('facebook/no_page_id_user'));
+    }
+    var params = {};
+
+    if( options.message )       params.message      = options.message;
+    if( options.picture )       params.picture      = options.picture;
+    if( options.link )          params.link         = options.link;
+    if( options.name )          params.name         = options.name;
+    if( options.caption )       params.caption      = options.caption;
+    if( options.description )   params.description  = options.description;
+    if( options.actions )       params.actions      = JSON.stringify(options.actions);
+
+    if( options.test ){
+        return callback(null, {result:123123123});
+    }
+
+    return facebook.graph('/'+options.object_id+'/feed',{
+        user: options.user,
+        params: params,
+        method:'post'
+    },function(error, result){
+        if( error ) return callback( error );
+        return callback(null, result);
+    });
+}
+
+/**
  * Get full info about a place by id
  *
  * fields is an array of requested fields. The following are permitted
