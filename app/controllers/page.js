e423da072d255fef11aa775d012a697085fa1d5d
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-23 16:22:29 -0400

    Cache busting again

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -67,7 +67,7 @@ exports.transfer_objects = {
             };
             page.is_place = page.location  && page.location.lat && page.location.lng;
             if( page.registered ){
-                page.image = burl('/page/'+page.id+'/image?v3');
+                page.image = burl('/page/'+page.id+'/image?v4');
             }
             if( user ){
                 page.like_url +='?token='+user.token;
