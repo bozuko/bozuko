fa7e810b71a16fff75b419fbcb35f1821c750576
Mark Fabrizio <fabrizim@owlwatch.com>
2011-06-23 16:13:29 -0400

    cache breaking

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -67,7 +67,7 @@ exports.transfer_objects = {
             };
             page.is_place = page.location  && page.location.lat && page.location.lng;
             if( page.registered ){
-                page.image = burl('/page/'+page.id+'/image?v2');
+                page.image = burl('/page/'+page.id+'/image?v3');
             }
             if( user ){
                 page.like_url +='?token='+user.token;
