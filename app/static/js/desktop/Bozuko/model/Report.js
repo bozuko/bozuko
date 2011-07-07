df6101e7ed547dd4e5d8f678a514b31bdc3732b5
Mark Fabrizio <fabrizim@owlwatch.com>
2011-07-07 10:40:44 -0400

    Remove the doRequest for now

--- app/static/js/desktop/Bozuko/model/Report.js
+++ app/static/js/desktop/Bozuko/model/Report.js
@@ -9,28 +9,6 @@ Ext.define('Bozuko.model.Report', {
         reader: {
             type: 'json',
             root: 'items'
-        },
-        doRequest: function(operation, callback, scope) {
-            var writer  = this.getWriter(),
-                request = this.buildRequest(operation, callback, scope);
-                
-            if (operation.allowWrite()) {
-                request = writer.write(request);
-            }
-            
-            Ext.apply(request, {
-                headers       : this.headers,
-                timeout       : this.timeout,
-                scope         : this,
-                callback      : this.createRequestCallback(request, operation, callback, scope),
-                method        : this.getMethod(request),
-                disableCaching: false // explicitly set it to false, ServerProxy handles caching
-            });
-            
-            if( this._activeRequest && this._activeRequest.abort ) this._activeRequest.abort();
-            this._activeRequest = Ext.Ajax.request(request);
-            
-            return request;
         }
     },
     
