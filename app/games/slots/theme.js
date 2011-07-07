5bdf145edfa19b81c08cd60fe3d56d55d1ee9d9e
Mark Fabrizio <fabrizim@owlwatch.com>
2011-07-07 12:40:01 -0400

    Change order of icons

--- app/games/slots/theme.js
+++ app/games/slots/theme.js
@@ -8,17 +8,17 @@ SlotsTheme.prototype.getListImage = function(){
 };
 
 SlotsTheme.prototype.icons = {
-    'bell'      :'bell.png',
-    'bar'       :'bar.png',
-    'cherry'    :'cherry.png',
-    'coins'     :'coins.png',
     'dollar'    :'dollar.png',
+    'coins'     :'coins.png',
+    'cherry'    :'cherry.png',
+    'bar'       :'bar.png',
+    'raygun'    :'raygun.png',
+    'rocket'    :'rocket.png',
+    'bell'      :'bell.png',
     'doublebar' :'doublebar.png',
     'gold'      :'gold.png',
     'horseshoe' :'horseshoe.png',
     'lemon'     :'lemon.png',
-    'raygun'    :'raygun.png',
-    'rocket'    :'rocket.png',
     'seven'     :'seven.png',
     'shamrock'  :'shamrock.png',
     'strawberry':'strawberry.png',
