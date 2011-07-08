f3f3a6300f90b3d18ebfde581f54b202935f2ea1
Playground User <playground@bozuko.com>
2011-07-08 18:41:20 -0400

    Changes to ensure that icons align with prizes

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -513,7 +513,7 @@ exports.routes = {
                 layout: false
             },
             handler : function(req, res){
-
+                
                 var transfer_objects = {};
                 var links = {};
                 Object.keys(Bozuko.transfers()).forEach(function(key){
@@ -523,6 +523,7 @@ exports.routes = {
                         def: transfer.def
                     }
                 });
+                
                 Object.keys(Bozuko.links()).forEach(function(key){
                     var link = Bozuko.link(key);
 
@@ -544,8 +545,8 @@ exports.routes = {
                         methods: methods
                     };
                 });
-
-                Bozuko.models.User.find({}, function(error, users){
+                
+                return Bozuko.models.User.find({}, {}, {limit:24}, function(error, users){
                     res.locals.transfer_objects = JSON.stringify(transfer_objects);
                     res.locals.links = JSON.stringify(links);
                     res.locals.users = JSON.stringify(users);
