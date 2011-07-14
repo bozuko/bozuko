553704fab06b33e6d14fbdc1e24d2cfde5260383
Mark Fabrizio <fabrizim@owlwatch.com>
2011-07-14 17:20:38 -0400

    Added the permission_denied tmpl

---
+++ app/static/js/desktop/Bozuko/view/contest/Reports.js
@@ -0,0 +1,20 @@
+Ext.define('Bozuko.view.contest.Reports', {
+    
+    alias : 'widget.contestreports',
+    extend : 'Ext.panel.Panel',
+    
+    initComponent : function(){
+        var me = this;
+        
+        Ext.apply(me, {
+            tbar : [{
+                
+            }]
+            
+            
+        });
+        
+        me.callParent( arguments );
+    }
+    
+});
