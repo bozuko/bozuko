ad30f50d973b8b1138adbbd7d7dd3c263a67807a
Mark Fabrizio <fabrizim@owlwatch.com>
2011-07-25 11:55:14 -0400

    Log headers to see if we can figure out what the user agent string is

--- app/controllers/facebook.js
+++ app/controllers/facebook.js
@@ -131,6 +131,8 @@ exports.routes = {
 
             handler : function(req, res){
                 
+                console.log(req.headers);
+                
                 var tmpl = ( /like_button/.test(req.url) ) ? 'like_button' : 'like';
                 if( tmpl == 'like_button' ){
                     res.locals.layout = false;
