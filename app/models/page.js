6a069c62c831ad60b85260dead4198fea0dde9e7
Mark Fabrizio <fabrizim@owlwatch.com>
2011-07-26 01:06:10 -0400

    typo - callback != cb

--- app/models/page.js
+++ app/models/page.js
@@ -368,7 +368,7 @@ Page.method('checkin', function(user, options, callback) {
                                  * We need to swallow this error as it can occur upon unsuccessful
                                  * validation and thus screw up the rest of our processing
                                  */
-                                return callback( null );
+                                return cb( null );
                             }
                             else{
                                 entries.push(entry);
