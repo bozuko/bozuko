61deb3e6adbf9be23f6f926367d72b2589d03167
Mark Fabrizio <fabrizim@owlwatch.com>
2011-08-02 00:54:04 -0400

    Added links and redirection for site

--- config/api.js
+++ config/api.js
@@ -14,9 +14,11 @@ module.exports = {
     client: {
         mobile:{
             iphone:{
+                link: 'http://itunes.com/app/bozuko',
                 min_version: '1.0'
             },
             android:{
+                link: 'https://market.android.com/details?id=com.bozuko.bozuko',
                 min_version: '1.0'
             }
         }
