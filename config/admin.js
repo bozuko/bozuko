83e0a22a2f79a263de0c446def6936fae8dbe441
Mark Fabrizio <fabrizim@owlwatch.com>
2011-08-05 00:07:47 -0400

    Add a new config for admin environment. redirect site environment /beta and /admin to new admin url. Remove oauth from required npm modules

--- config/admin.js
+++ config/admin.js
@@ -4,7 +4,8 @@ module.exports = {
 
     controllers: {
         only: [
-            'site',
+            'beta',
+            'admin',
             'mycroft'
         ]
     },
@@ -102,17 +103,11 @@ module.exports = {
         auth: false,
         ssl: true,
 		ssl_config:{
-            /*
             key : '/ssl/wildcard/wildcard.bozuko.com.key',
 			ca : '/ssl/wildcard/gd_bundle.crt',
 			cert: '/ssl/wildcard/bozuko.com.crt'
-            */
-			key : '/ssl/bozuko.com/bozuko.com.key',
-			ca : '/ssl/bozuko.com/gd_bundle.crt',
-			cert: '/ssl/bozuko.com/bozuko.com.crt'
-            
 		},
-        host: 'bozuko.com',
+        host: 'admin.bozuko.com',
         port: 443
     },
 
