6ebf0daefd60abb0229526e2f4a709219f74dd5e
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-08-09 00:05:03 -0400

    initial contest validation

---
+++ app/models/test/contest_validation.js
@@ -0,0 +1,108 @@
+var testsuite = require('./config/testsuite');
+var async = require('async');
+var BozukoError = Bozuko.require('core/error');
+
+var start = new Date();
+var end = new Date();
+end.setTime(+start+(1000*60*60*24*2));
+
+var page = new Bozuko.models.Page();
+page.active = true;
+page.name = "Test validation page";
+
+var contest = new Bozuko.models.Contest(
+{
+    page_id: page._id,
+    engine_type: 'order',
+    game: 'slots',
+    game_config: {
+        theme: 'default'
+    },
+    entry_config: [{
+        type: "facebook/checkin",
+        tokens: 3,
+        duration: 2
+    }],
+    start: start,
+    end: end,
+    win_frequency: 1.4,
+    free_play_pct: 30
+});
+
+exports['save page'] = function(test) {
+    page.save(function(err) {
+        test.ok(!err);
+        test.done();
+    });
+};
+
+
+exports['prizes - no prizes'] = function(test) {
+    contest.validatePrizes(function(err, status) {
+        test.ok(status.errors[0] instanceof BozukoError);
+        test.ok(status.errors[0].name === 'validate/contest/no_prizes');
+        test.done();
+    });
+};
+
+exports['prizes - valid prize'] = function(test) {
+    contest.prizes.push({
+        name: 'giftcard',
+        value: '0',
+        description: 'Gonna create some sick desynes fer you',
+        details: 'Don\'t worry, you won\'t make money off this',
+        instructions: 'Check yer email fool!',
+        total: 1,
+        won: 0,
+        redeemed: 0,
+        is_email: true,
+        email_body: 'Something',
+        email_codes: ["15h1ttyd3s1gn"]
+    });
+
+    var email_codes, email_body;
+    contest.validatePrizes(function(err, status) {
+        for (var i = 0; i < status.errors.length; i++) {
+	    if (status.errors[i].name === 'validate/contest/email_codes') {
+		email_codes = true;
+	    }
+	    if (status.errors[i].name === 'validate/contest/email_body') {
+		email_body = true;
+	    }
+	}
+        test.ok(!email_codes);
+        test.ok(!email_body);
+        test.done();
+    });
+};
+
+exports['prizes - email codes count and missing body'] = function(test) {
+    contest.prizes[0].total = 2;
+    contest.prizes[0].email_body = '';
+
+    var email_codes, email_body;
+
+    contest.validatePrizes(function(err, status) {
+        for (var i = 0; i < status.errors.length; i++) {
+	    if (status.errors[i].name === 'validate/contest/email_codes') {
+		email_codes = true;
+	    }
+	    if (status.errors[i].name === 'validate/contest/email_body') {
+		email_body = true;
+	    }
+	}
+        test.ok(email_codes);
+        test.ok(email_body);
+        test.done();
+    });
+};
+
+exports['entries and plays'] = function(test) {
+
+    contest.publish(function(err) {
+        contest.validateEntriesAndPlays(function(err, status) {
+            test.ok(!status);
+            test.done();
+        });
+    });
+};
