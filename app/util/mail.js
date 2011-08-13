ad68f981d68c5e195152b69f4462d1dd1d20d2a9
Mark Fabrizio <fabrizim@playground.bozuko.com>
2011-08-13 13:14:07 -0400

    Added retries to mailer

--- app/util/mail.js
+++ app/util/mail.js
@@ -13,6 +13,27 @@ EmailMessage.prototype.__proto__ = nm.EmailMessage.prototype;
 
 // static method (emulate the nodemailer send_mail)
 EmailMessage.send = function(params, callback){
-    var em = new EmailMessage(params);
-    em.send(callback);
+    
+    var em = new EmailMessage(params),
+        attempts = 0;
+        
+    var attempt = function(error){
+        
+        if( !error ){
+            return callback.apply(arguments);
+        }
+        
+        attempts++;
+        if( error && attempts > 3 ){
+            return callback.apply(arguments);
+        }
+        
+        // retry (wait 10 seconds the first time, 20 seconds the second time, 30 seconds the third time)
+        return setTimeout(function(){
+            em.send(attempt);
+        }, attempts * 10000);
+        
+    };
+    
+    em.send(attempt);
 };
