fe80a8e71bc8324a1088b7131a1cd520cdde78ea
Mark Fabrizio <fabrizim@playground.bozuko.com>
2011-08-16 12:50:38 -0400

    Added new Prizes display - need to figure out how to get expired / active

--- app/static/js/desktop/Bozuko/view/contest/Prizes.js
+++ app/static/js/desktop/Bozuko/view/contest/Prizes.js
@@ -1,63 +1,34 @@
 Ext.define('Bozuko.view.contest.Prizes', {
     
-    extend              :'Ext.panel.Panel',
+    extend              :'Ext.grid.Panel',
     alias               :'widget.contestprizes',
     
     initComponent       :function(){
         var me = this;
         
-        
         Ext.apply( me, {
-            
             autoScroll      :true,
-            
-            items           :[{
-                xtype           :'dataview',
-                store           :me.contest.prizes(),
-                
-                cls             :'prize-list bozuko-list',
-                disableSelection:true,
-                
-                itemTpl         :new Ext.XTemplate(
-                    '<div>',
-                        '<div class="numbers">',
-                            '<table>',
-                                '<tr><th>Total:</th><td><span class="number">{total}</span></td></tr>',
-                                '{[this.getWon(values)]}',
-                                '{[this.getRedeemed(values)]}',
-                                '{[this.getRemaining(values)]}',
-                            '</table>',
-                        '</div>',
-                        '<div class="title">{name}</div>',
-                        '<table class="info">',
-                            '<tr valign="top"><th>Value:</th><td>${value}</td></tr>',
-                            '<tr valign="top"><th>Description:</th><td>{description}</td></tr>',
-                            '<tr valign="top"><th>Instructions:</th><td>{instructions}</td></tr>',
-                        '</table>',
-                    '</div>',
-                    {
-                        getWon : function(values){
-                            return this.getNumberString(values.total, values.won, 'Won');
-                        },
-                        getRedeemed : function(values){
-                            return this.getNumberString(values.total, values.redeemed, 'Redeemed');
-                        },
-                        getRemaining : function(values){
-                            return this.getNumberString(values.total, values.total-values.won, 'Remaining');
-                        },
-                        getNumberString : function(total, number, string){
-                            var percent = (number/total*100).toFixed(2);
-                            return '<tr><th>'+string+': </th><td><span class="number">'+number+'</span> <span class="percent">'+percent+'%</span></td></tr>';
-                        }
-                    }
-                )
-            }]
+            store           :me.contest.prizes(),
+            columns: [
+                {header: 'Name',            dataIndex: 'name',          flex: 1},
+                {header: 'Total Quantity',  dataIndex: 'total'},
+                {header: 'Total Remaining', dataIndex: 'total',         renderer:me.renderers.remaining},
+                {header: 'Won',             dataIndex: 'won'},
+                {header: 'Active',          dataIndex: 'won',           renderer:me.renderers.active},
+                {header: 'Redeemed',        dataIndex: 'redeemed'}
+            ]
         });
-        
-        me.callParent( arguments );
-        me.dataview = me.down('dataview');
-        
-        me.contest.on('update', me.dataview.refresh, me.dataview);
+            
+        me.callParent(arguments);
+    },
+    
+    renderers : {
+        remaining : function(value, meta, record){
+            return record.get('total') - record.get('won');
+        },
+        active : function(value, meta, record){
+            return (record.get('won') - record.get('redeemed'))+'<sup>*</sup>';
+        }
     }
     
 });
