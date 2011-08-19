0510ab0290e6ec022cb736e4f3238040a6da1038
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-08-18 20:49:21 -0400

    add charm and multimeter to the install

--- install/install.sh
+++ install/install.sh
@@ -14,6 +14,13 @@ install_node
 echo "export PATH=~/bozuko/bin:~/node_modules/.bin/:$PATH" >> ~/.bashrc
 source ~/.bashrc
 
+# Install multimeter. Use normal npm install when bar.prototype.ratio patch is accepted.
+git clone git@github.com:andrewjstone/node-multimeter.git
+npm install node-multimeter
+rm -rf node-multimeter
+
 # node packages
 echo "*** Installing node packages with npm"
-cd ~ && npm install connect connect-auth express jade less mongodb mongoose@1.8.0 monomi qs socket.io markdown-js async sprintf nodeunit nodemailer dateformat relative-date cluster imap node-uuid node-gd knox validator
+cd ~ && npm install connect connect-auth express jade less mongodb mongoose@1.8.0 monomi   \
+    qs socket.io markdown-js async sprintf nodeunit nodemailer dateformat relative-date    \
+    cluster imap node-uuid node-gd knox validator charm 
