9099b7809402ff2bdacc9653d27d4709939bda4f
Mark Fabrizio <fabrizim@playground.bozuko.com>
2011-08-23 19:33:16 -0400

    mark, you dumbass

--- app/models/page.js
+++ app/models/page.js
@@ -285,7 +285,7 @@ Page.method('canUserCheckin', function(user, callback){
                 if( distance > allowed_distance ){
                     // how much longer?
                     var time = (distance / Bozuko.cfg('checkin.travel.speed', 60)) * DateUtil.HOUR;
-                    console.error('User can check in here '+DateUtil.inAgo( new Date(+checkin.timestamp+time)) );
+                    console.error('User can check in here '+DateUtil.inAgo( new Date(+start+time)) );
                     
                     return callback( null, false, checkin, Bozuko.error('checkin/too_far_too_soon') );
                 }
