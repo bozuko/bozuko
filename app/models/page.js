7a96b07cf67971284ae1b448316189eb57d6bcb4
Mark Fabrizio <fabrizim@playground.bozuko.com>
2011-08-23 19:34:36 -0400

    Fixed the checkin distance / removed the logging

--- app/models/page.js
+++ app/models/page.js
@@ -279,8 +279,6 @@ Page.method('canUserCheckin', function(user, callback){
                     // total distance between places
                     distance = Geo.distance(self.coords, checkin.coords, 'mi');
                     ;
-                    
-                console.error( [start, hours, allowed_distance, distance].join(',') );
                 
                 if( distance > allowed_distance ){
                     // how much longer?
