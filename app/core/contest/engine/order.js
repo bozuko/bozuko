4f00e10d7e33670bbe8a513050eaff250ae8d762
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-08-24 13:37:34 -0400

    add shaped odds

--- app/core/contest/engine/order.js
+++ app/core/contest/engine/order.js
@@ -7,27 +7,46 @@ var OrderEngine = module.exports = function(){
 };
 inherits( OrderEngine, Engine );
 
-OrderEngine.prototype.generateResults = function( ){
+OrderEngine.prototype.default_primer_end = 0.10;
+
+OrderEngine.prototype.generateResults = function(){
+
+    options = this.contest.engine_options || {};        
 
     var contest = this.contest;
     var results = {};
 
     var prizes = contest.prizes;
 
-    var max=0;
-    contest.entry_config.forEach(function(entry_config){
-        var entryMethod = Bozuko.entry(entry_config.type, contest);
-        entryMethod.configure( entry_config );
-       max = Math.max(parseInt(entryMethod.getMaxTokens()),max);
-    });
-
+    var entryConfig = contest.getEntryConfig();
+    var entryMethod = Bozuko.entry(entryConfig.type, contest);
+    entryMethod.configure( entryConfig );
+    var max = entryMethod.getMaxTokens();
+    
     var totalPlays = max*contest.total_entries;
     var freePlays = Math.floor(contest.free_play_pct/100*totalPlays) || 0;
     totalPlays = totalPlays + freePlays;
 
-    // not sure if this is the best way to do this, but it works
-    var ar = [];
-    for( var i=0; i<totalPlays; i++) ar.push(i);
+    var ar = [],
+        primer = [],
+        remainder = [];
+    
+    // By default set the primer_end to 10%. Single prizes will go out after primer_end even if
+    // contest.engine_options.primer is not set.
+    var primer_end = Math.floor(this.default_primer_end*totalPlays);
+
+    // Build Arrays that contain the indexes for results (ar, primer, remainder)
+    if (options.primer) {
+        primer_end = Math.floor(options.primer.region/100 * totalPlays);
+        for (var j = 0; j < primer_end; j++) {
+            primer.push(j);
+        }
+        for (j = primer_end; j < totalPlays; j++) {
+            remainder.push(j);
+        }
+    } else {
+        for( var i=0; i<totalPlays; i++) ar.push(i);
+    }
 
     // Store all generated prize codes for this contest so we don't have dupes.
     var codes = {'0' : true};
@@ -42,32 +61,72 @@ OrderEngine.prototype.generateResults = function( ){
         return code;
     }
 
-    function pick_index() {
-        var random = rand(0,ar.length-1);
-        var index = ar[random];
-        ar.splice( random, 1 );
+    function pick_index(array, start) {
+        var random = rand(start || 0,array.length-1);
+        var index = array[random];
+        array.splice( random, 1 );
         return index;
     }
 
+
+    // Distribute prizes in the results object
     var index;
     prizes.forEach(function(prize, prize_index){
-        for( var i = 0; i < prize.total; i++ ){
-            index = pick_index();
+        
+        if (options.primer) {
+            var primer_prize_ct = Math.floor(options.primer.density/100 * prize.total);
+        }
+
+        if (options.primer && prize.total === 1) {
+            index = pick_index(remainder);
             results[index] = {
                 index: prize_index,
                 prize: prize._id,
                 code: get_code(),
-                count: i
+                count: 0
+            };         
+        } else if (!options.primer && prize.total === 1) {
+            index = pick_index(ar, primer_end);
+            results[index] = {
+                index: prize_index,
+                prize: prize._id,
+                code: get_code(),
+                count: 0
             };
+        } else {
+            for( var i = 0; i < prize.total; i++ ){
+                if (options.primer) {
+                    if (i < primer_prize_ct) {
+                        index = pick_index(primer);
+                    } else {
+                        index = pick_index(remainder);
+                    }
+                } else {
+                    index = pick_index(ar);
+                }
+                results[index] = {
+                    index: prize_index,
+                    prize: prize._id,
+                    code: get_code(),
+                    count: i
+                };
+            }
         }
     });
 
+    // Use remaining indexes for free play distribution
+    if (options.primer) ar = primer.concat(remainder);
+
+    // Distribute free plays
     for (i = 0; i < freePlays; i++) {
-        index = pick_index();
+        index = pick_index(ar);
         results[index] = 'free_play';
     }
 
+    // Remove index arrays
     delete ar;
+    delete primer;
+    delete remainder;
 
     contest.results = results;
     contest.total_plays = totalPlays;
