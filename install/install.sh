42432179aa559eeac716701d77b13b4003d48d9c
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-09-05 21:24:19 -0400

    custom mongoose in install

--- install/install.sh
+++ install/install.sh
@@ -20,6 +20,10 @@ cd ~ && npm install connect connect-auth express jade less mongodb mongoose@2.1.
     qs socket.io markdown-js async sprintf nodeunit nodemailer dateformat relative-date    \
     cluster imap node-uuid node-gd knox validator charm connect-form
 
+# install my version of mongoose so safe values get passed through properly.
+git clone git@github.com:andrewjstone/mongoose.git
+npm install mongoose
+rm -rf mongoose
 
 # install my version of node-mongodb-native so w:2 works. Remove when patch is accepted.
 git clone git@github.com:andrewjstone/node-mongodb-native.git
