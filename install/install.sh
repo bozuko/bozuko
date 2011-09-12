e45c1aa09495ef1ec21ccdab3708a43a8bcb351d
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-09-11 21:10:42 -0400

    config and install updateds

--- install/install.sh
+++ install/install.sh
@@ -16,17 +16,9 @@ source ~/.bashrc
 
 # node packages
 echo "*** Installing node packages with npm"
-cd ~ && npm install connect connect-auth express jade less mongodb mongoose@2.1.0 monomi   \
+cd ~ && npm install connect connect-auth express jade less mongodb@0.9.6-15 mongoose@2.1.2 monomi   \
     qs socket.io markdown-js async sprintf nodeunit nodemailer dateformat relative-date    \
     cluster imap node-uuid node-gd knox validator charm connect-form xml2json
 
-# install my version of mongoose so safe values get passed through properly.
-git clone git@github.com:andrewjstone/mongoose.git
-npm install mongoose
-rm -rf mongoose
-
-# install my version of node-mongodb-native so w:2 works. Remove when patch is accepted.
-git clone git@github.com:andrewjstone/node-mongodb-native.git
-cp -R node-mongodb-native/* ~/node_modules/mongodb/
-cp -R node-mongodb-native/* ~/node_modules/mongoose/node_modules/mongodb/
-rm -rf node-mongodb-native
+# copy latest mongodb to mongoose dir so w:2 works
+cp -R ~/node_modules/mongodb/* ~/node_modules/mongoose/node_modules/mongodb
