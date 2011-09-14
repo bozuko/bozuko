4cd213878273f0258248f8c7847ef6b6227afcd7
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-09-14 14:26:00 -0400

    add dummy paypal controller

---
+++ app/controllers/paypal.js
@@ -0,0 +1,23 @@
+var inspect = require('util').inspect;
+var http = Bozuko.require('util/http');
+
+exports.routes = {
+    '/ipn': {
+        post: {
+            handler: function(req, res) {
+                if (Bozuko.env === 'playground') {
+                    http.request({
+                        url: 'https://playground.bozuko.com:8004/ipn',
+                        method: 'POST',
+                        headers: req.headers,
+                        body: req.rawBody
+                    }, function() {
+                        console.log("Proxied IPN req to dev because Paypal sucks");
+                    });
+                } else {
+                    console.log("IPN = "+inspect(req));
+                }
+            }
+        }
+    }
+};
