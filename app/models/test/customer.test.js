43aada0c503304b3761bd7addd37eda30f464d7b
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-09-22 19:03:49 -0400

    more flexible use of gateway. Support updating subscriptions

--- app/models/test/customer.test.js
+++ app/models/test/customer.test.js
@@ -3,6 +3,8 @@ var BraintreeGateway = Bozuko.require('util/braintree');
 var inspect = require('util').inspect;
 var async = require('async');
 
+var gateway = new BraintreeGateway().gateway;
+
 var customer, 
     cc_token,
     page
@@ -19,14 +21,14 @@ exports['create page'] = function(test) {
 };
 
 exports['find customer - fail missing'] = function(test) {
-    Bozuko.models.Customer.findByGatewayId(page._id, function(err, result) {
+    Bozuko.models.Customer.findByGatewayId(gateway, page._id, function(err, result) {
         test.ok(err);
         test.done();
     });
 };
 
 exports['create customer - success'] = function(test) {
-    Bozuko.models.Customer.create({
+    Bozuko.models.Customer.create(gateway, {
         page_id: page._id,
         firstName: 'Murray',
         lastName: 'Rothbard',
@@ -41,7 +43,7 @@ exports['create customer - success'] = function(test) {
 };
 
 exports['create same customer - fail'] = function(test) {
-    Bozuko.models.Customer.create({
+    Bozuko.models.Customer.create(gateway, {
         page_id: page._id,
         firstName: 'Murray',
         lastName: 'Rothbard',
@@ -54,10 +56,9 @@ exports['create same customer - fail'] = function(test) {
 };
 
 exports['create same customer - success after removal from braintree'] = function(test) {
-    var gateway = new BraintreeGateway().gateway;
     gateway.customer.delete(String(page._id), function(err) {
         test.ok(!err);
-        Bozuko.models.Customer.findByGatewayId(page._id, function(err, result) {
+        Bozuko.models.Customer.findByGatewayId(gateway, page._id, function(err, result) {
             test.ok(err);
             exports['create customer - success'](test);
         });
@@ -65,7 +66,7 @@ exports['create same customer - success after removal from braintree'] = functio
 };
 
 exports['find customer'] = function(test) {
-    Bozuko.models.Customer.findByGatewayId(page._id, function(err, result) {
+    Bozuko.models.Customer.findByGatewayId(gateway, page._id, function(err, result) {
         test.ok(!err);
         test.done();
     });
@@ -74,7 +75,6 @@ exports['find customer'] = function(test) {
 // DO NOT EVER do this on a production system. This is only for tests. 
 // We must use Transparent Redirect (TR) to remain PCI compliant.
 exports['create credit card'] = function(test) {
-    var gateway = new BraintreeGateway().gateway;
     gateway.creditCard.create({
         customerId: String(customer.page_id),
         number: "5105105105105100",
@@ -91,7 +91,7 @@ exports['create credit card'] = function(test) {
 // If the user wants to update their subscriptions/cc info we must retrieve it all from braintree.
 // We don't store it in mongo.
 exports['find customer - retrieve credit card token'] = function(test) {
-    Bozuko.models.Customer.findByGatewayId(customer.page_id, function(err, result) {
+    Bozuko.models.Customer.findByGatewayId(gateway, customer.page_id, function(err, result) {
         test.ok(!err);
         test.deepEqual(cc_token, result.creditCards[0].token);
         test.done();
@@ -99,7 +99,7 @@ exports['find customer - retrieve credit card token'] = function(test) {
 };
 
 exports['create active subscription - success'] = function(test) {
-    customer.createActiveSubscription({
+    customer.createActiveSubscription(gateway, {
         paymentMethodToken: cc_token,
         planId: 'monthly_subscription'
     }, function(err, result) {
@@ -109,7 +109,7 @@ exports['create active subscription - success'] = function(test) {
 };
 
 exports['create another active subscription - fail'] = function(test) {
-    customer.createActiveSubscription({
+    customer.createActiveSubscription(gateway, {
         paymentMethodToken: cc_token,
         planId: 'monthly_subscription'
     }, function(err, result) {
@@ -119,7 +119,7 @@ exports['create another active subscription - fail'] = function(test) {
 };
 
 exports['cancel active subscription'] = function(test) {
-    customer.cancelActiveSubscription(function(err, result) {
+    customer.cancelActiveSubscription(gateway, function(err, result) {
         test.ok(!err);
         test.done();
     });
@@ -127,17 +127,18 @@ exports['cancel active subscription'] = function(test) {
 
 // We always allow cancelling subscriptions. Only if there is a service error will an error be returned.
 exports['cancel active subscription again - success'] = function(test) {
-    customer.cancelActiveSubscription(function(err) {
+    customer.cancelActiveSubscription(gateway, function(err) {
         test.ok(!err);
         test.done();
     });
 };
 
+
 exports['create another active subscription with sub on mongo but not braintree - success'] = function(test) {
     customer.subscriptions.push({active:true});
     customer.save(function(err) {
         test.ok(!err);
-        customer.createActiveSubscription({
+        customer.createActiveSubscription(gateway, {
             paymentMethodToken: cc_token,
             planId: 'monthly_subscription'
         }, function(err, result) {
@@ -147,23 +148,36 @@ exports['create another active subscription with sub on mongo but not braintree 
     });
 };
 
+exports['update active subscription - success'] = function(test) {
+    var opts = {
+        price: '125.00',
+        options: {
+            prorateCharges: true
+        }
+    };
+    customer.updateActiveSubscription(gateway, opts, function(err, result) {
+        test.ok(!err);
+        test.deepEqual(result.subscription.price, '125.00');
+        test.done();
+    });
+};
+
 exports['cancel subscription with active sub in braintree but no active sub in mongo - success'] = function(test) {
     customer.subscriptions[customer.subscriptions.length-1].active = false;
-    customer.cancelActiveSubscription(function(err) {
+    customer.cancelActiveSubscription(gateway, function(err) {
         test.ok(!err);
         test.done();
     });
 };
 
 exports['cancel the last subscription added - success'] = function(test) {
-    customer.cancelActiveSubscription(function(err) {
+    customer.cancelActiveSubscription(gateway, function(err) {
         test.ok(!err);
         test.done();
     });
 };
 
 exports['verify all subscriptions are cancelled'] = function(test) {
-    var gateway = new BraintreeGateway().gateway;
     test.equal(customer.subscriptions.length, 2);
     async.forEach(customer.subscriptions, function(sub, cb) {
         test.ok(!sub.active);
@@ -177,10 +191,17 @@ exports['verify all subscriptions are cancelled'] = function(test) {
     });
 };
 
+exports['update active subscription - fail'] = function(test) {
+    var opts = {price: '150.00'};
+    customer.updateActiveSubscription(gateway, opts, function(err, result) {
+        test.ok(err);
+        test.done();
+    });    
+};
+
 // Not sure we actually ever want to remove customers. We may just want to cancel subscriptions.
 // So just do it from the test for cleanup.
 exports['remove customer'] = function(test) {
-    var gateway = new BraintreeGateway().gateway;
     gateway.customer.delete(String(customer.page_id), function(err, result) {
         test.ok(!err);
         customer.remove(function(err) {
@@ -194,7 +215,7 @@ exports['remove customer'] = function(test) {
 };
 
 exports['verify customer doesn\'t exist'] = function(test) {
-    Bozuko.models.Customer.findByGatewayId(customer._id, function(err, result) {
+    Bozuko.models.Customer.findByGatewayId(gateway, customer._id, function(err, result) {
         test.ok(err);
         test.equal(err.type, 'notFoundError');
         test.ok(!result);
