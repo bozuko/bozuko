653d5e7b68124a87562b6c666a00b7ebc58064d6
Mark Fabrizio <mark.fabrizio@bozuko.com>
2011-10-23 10:29:50 -0400

    Chage interval

--- app/core/report/interval.js
+++ app/core/report/interval.js
@@ -57,13 +57,15 @@ CountsReport.prototype.run = function run(callback){
             break;
         
         case 'Week':
-            end = new Date( Date.UTC(end.getUTCFullYear(),end.getUTCMonth(),end.getUTCDate()+1) );
+            end = new Date( Date.UTC(end.getUTCFullYear(),end.getUTCMonth(),end.getUTCDate()) );
             // check to see which day of the week this falls on..
             var d = end.getDay();
+            
             if( d !== 0 ){
                 var i = 7-d;
-                end = new Date( Date.UTC(end.getUTCFullYear(),end.getUTCMonth(),end.getUTCDate()+i) );
+                end = new Date( Date.UTC(end.getUTCFullYear(),end.getUTCMonth(),end.getUTCDate()+i+1) );
             }
+            
             start = new Date( Date.UTC(end.getUTCFullYear(),end.getUTCMonth(),end.getUTCDate()-(length*7)) );
             break;
         
