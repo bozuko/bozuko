141fc532d009582d189d5cd7c0320c305a586059
Mark Fabrizio <fabrizim@playground.(none)>
2011-11-08 23:10:08 -0500

    Fix like button for places that have restrictions

--- app/static/js/client/App.js
+++ app/static/js/client/App.js
@@ -37,36 +37,88 @@ Bozuko.client.App = Ext.extend( Ext.util.Observable, {
         var self = this,
             api = this.api;
             
+        //initialize facebook
+        FB.init({
+            appId: Bozuko.client.App.facebookApp.id, 
+            status: true, 
+            cookie: true,
+            xfbml: true,
+            oauth: true
+        });
+      
+        FB.Event.subscribe('auth.login', function(data){
+            self.onFacebookLogin(data)
+        });
+        FB.Event.subscribe('auth.logout', function(data){
+            self.onFacebookLogout(data)
+        });
+        
+        FB.getLoginStatus(function(response){
+            self.login(function(){
+                
+            });
+        });
+        
         api.call(function(response){
             
             self.entry_point = response.data;
             
             if( !api.last().data.links.user ){
-                self.startFromPath();
+                // self.startFromPath();
             }
             else{
                 api.call({path: api.last().data.links.user},function(response){
                     self.user = response.data;
                     api.setToken(self.user.token);
-                    self.startFromPath();
+                    // dself.startFromPath();
                 });
             }
         });
     },
     
     login : function(callback){
+        var self = this;
+        this.loginCallback = callback;
+        
         if( !this.$loginScreen ){
             this.$loginScreen = this.$modal.createChild({
-                cls: 'login',
+                cls: 'modal-dialog login',
                 cn:[{
                     cls: 'facebook-btn',
                     tag: 'button',
                     html: 'Login'
                 }]
             });
-            
+            this.$loginScreen.down('.facebook-btn').on('touchstart', function(){
+                self.fbLogin();
+            });
             this.$modal.show();
         }
+    },
+    
+    fbLogin : function(){
+        alert('start');
+        FB.login(function(){
+            alert('done');
+        });
+    },
+    
+    onFacebookLogin : function(data){
+        
+    },
+    onFacebookLogout: function(data){
+        
+    },
+    
+    createModalWindow : function(cfg){
+        var $window = this.$modal.createChild({
+            cls: 'modal-window'
+        });
+        return $window;
+    },
+    
+    showModalWindow : function(window){
+        this.$modal.show();
     },
     
     startFromPath : function(){
@@ -80,7 +132,6 @@ Bozuko.client.App = Ext.extend( Ext.util.Observable, {
         else{
             alert('Path Not Supported');
         }
-        
     },
     
     openGame : function( path ){
@@ -104,8 +155,6 @@ Bozuko.client.App = Ext.extend( Ext.util.Observable, {
                     alert('Unsupported Game Type');
                     return;
                 }
-                
-                console.log( Bozuko.client.game.Scratch );
                 
                 self.scratch = new Bozuko.client.game.Scratch({
                     game: gameResponse.data,
