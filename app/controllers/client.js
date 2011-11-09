9d7fc728e10dc4e287fec9fed4ef39993919f309
Mark Fabrizio <fabrizim@playground.(none)>
2011-11-08 23:10:08 -0500

    Fix like button for places that have restrictions

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -1,3 +1,5 @@
+var Facebook = Bozuko.require('util/facebook');
+
 // exports.access = 'admin';
 
 exports.init = function(){
@@ -9,6 +11,8 @@ exports.locals = {
     device : 'desktop'
 };
 
+exports.session = false;
+
 exports.routes = {
     '/client' : {
         alias: '/client/*',
@@ -19,10 +23,35 @@ exports.routes = {
             }
         }
     },
-    '/client/login' : {
-        get : {
+    '/client/fblogin' : {
+        post : {
             handler : function(req, res){
                 
+                var ret = {success: false};
+                
+                var token = req.param('token');
+                if( !token ){
+                    return res.send( ret );
+                }
+                // else lets see if we can find this guy...
+                return Facebook.api('/me', {params:{
+                    access_token: token
+                }}, function(error, result){
+                    
+                    if( error || !result ) return res.send(ret);
+                    
+                    // see if this is an existing person...
+                    return Bozuko.models.User.findByService('facebook', result.id, function(error, user){
+                        
+                        if( error ) return res.send(ret);
+                        
+                        return Bozuko.transfer('user', user, user, function(error, result){
+                            res.send( error || result );
+                        });
+                        
+                    });
+                    
+                });
             }
         }
     }
