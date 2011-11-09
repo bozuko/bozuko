9df2ea9e109a2a66179744d414e15c16002216ff
Mark Fabrizio <fabrizim@playground.(none)>
2011-11-09 00:21:43 -0500

    Add gold

---
+++ app/games/scratch/themes/gold/index.js
@@ -0,0 +1,18 @@
+var ScratchTheme = require('./../../theme'),
+    burl = Bozuko.require('util/url').create,
+    path = require('path'),
+    inherits = require('util').inherits;
+
+var GoldTheme = module.exports = function(game){
+    ScratchTheme.apply(this, arguments);
+};
+
+inherits(GoldTheme, ScratchTheme);
+
+GoldTheme.prototype.name = 'gold';
+GoldTheme.prototype.images = {
+    'background'    :'background.png'
+};
+
+GoldTheme.prototype.base = 'https://s3.amazonaws.com/bozuko/public/scratch/themes/gold/theme';
+GoldTheme.prototype.icon = 'https://s3.amazonaws.com/bozuko/public/scratch/themes/gold/scratch.png';
