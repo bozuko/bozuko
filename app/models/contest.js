3c2fbc097f89ab84d4b0fa34bbb6467f9ce128e1
Andrew Stone <andrew.stone@bozuko.com>
2011-11-18 18:16:07 -0500

    use self instead of this

--- app/models/contest.js
+++ app/models/contest.js
@@ -557,8 +557,8 @@ Contest.method('sendEndOfGameAlert', function(page) {
         return mail.send({
             to: to,
             cc: 'dev@bozuko.com',
-            subject: 'You Bozuko Contest \''+this.name+'\' is about to expire!',
-            body: 'You Bozuko Contest \''+this.name+'\' is about to expire!\n'
+            subject: 'You Bozuko Contest \''+self.name+'\' is about to expire!',
+            body: 'You Bozuko Contest \''+self.name+'\' is about to expire!\n'
                 + 'Please login to your Bozuko account at bozuko.com/beta to create a new contest.'
         }, function(err, success, record) {
             if (err || !success) {
