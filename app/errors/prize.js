901eb497e4ccbaa98664c3e06813437fe9080989
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-12-12 01:15:44 -0500

    functional verification

--- app/errors/prize.js
+++ app/errors/prize.js
@@ -4,8 +4,9 @@ module.exports = {
         code: 400,
         message: 'state parameter must be one of \'active\', \'redeemed\', or \'expired\''
     },
-    
+
     already_redeemed: "This Prize has already been redeemed",
+    already_verified: "This Prize has already been verified",
     expired: "This Prize has expired",
     redeem_bad_user: "Current user cannot redeem another users prize"
 };
