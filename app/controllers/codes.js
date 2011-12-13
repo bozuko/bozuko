970f588a64b3c80a99420315c6a3a6783642f801
Andrew Stone <andrew.j.stone.1@gmail.com>
2011-12-13 15:42:24 -0500

    better error handling and some css mods

--- app/controllers/codes.js
+++ app/controllers/codes.js
@@ -29,7 +29,6 @@ exports.locals = {
     styles: [
         '/css/desktop/style.css?'+Date.now(),
         '/css/desktop/layout.css?'+Date.now(),
-        '/css/desktop/beta/style.css?'+Date.now(),
         '/css/desktop/codes.css?'+Date.now()
     ]
 };
@@ -80,6 +79,7 @@ exports.routes = {
                         return res.render('codes/index.jade');
                     });
                 }
+                res.locals.error_ctx = 'codes';
                 res.locals.pin = pin;
                 res.locals.page_id = page_id;
                 return Bozuko.models.Prize.findOne({code: code, page_id: page_id}, function(err, prize) {
@@ -97,7 +97,21 @@ exports.routes = {
         post: {
             handler: function(req, res) {
                 var code = req.param('code');
-                return Bozuko.models.Prize.findOne({code: code}, function(err, prize) {
+                var pin = req.param('pin');
+                var page_id = req.param('page_id');
+                if (!pin) return res.render('codes/pin.jade');
+                if (!page_id) {
+                    return Bozuko.models.Page.verifyPin(pin, function(err, page) {
+                        if (err) return error(res, err);
+                        res.locals.pin = pin;
+                        res.locals.page_id = page._id;
+                        return res.render('codes/index.jade');
+                    });
+                }
+                res.locals.error_ctx = 'codes';
+                res.locals.pin = pin;
+                res.locals.page_id = page_id;
+                return Bozuko.models.Prize.findOne({code: code, page_id:  page_id}, function(err, prize) {
                     if (err) return error(res, err);
                     if (!prize) return res.render('codes/not_found.jade');
                     res.locals.code = code;
