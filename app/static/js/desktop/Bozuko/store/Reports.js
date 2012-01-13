be227ce5e47ac6f6ff379017b7f500bad464b25f
Mark Fabrizio <fabrizim@playground.(none)>
2012-01-12 22:37:53 -0500

    Start to change charts over

--- app/static/js/desktop/Bozuko/store/Reports.js
+++ app/static/js/desktop/Bozuko/store/Reports.js
@@ -2,7 +2,30 @@ Ext.define('Bozuko.store.Reports', {
     extend: 'Ext.data.Store',
     
     requires: ['Bozuko.model.Report','Ext.data.reader.Json'],
+    
+    proxy: {
+        type: 'rest',
+        url: '/report',
+        reader: {
+            type: 'json',
+            root: 'items',
+            getResponseData : function(){
+                var data = Ext.data.reader.Json.prototype.getResponseData.apply(this, arguments);
+                this._unit = data.unit;
+                this._label = data.label;
+            }
+        }
+    },
 
     model: 'Bozuko.model.Report',
-    autoLoad: true
+    autoLoad: true,
+    
+    getUnit : function(){
+        return this._unit || 'day';
+    },
+    
+    getLabel : function(){
+        return this._label || 'Day';
+    }
+    
 });
