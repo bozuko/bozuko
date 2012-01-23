022c4f28d04a58e952473e49e76b000e00f926fc
Mark Fabrizio <fabrizim@playground.(none)>
2012-01-23 12:13:13 -0500

    Default to Like. Make it the first option

--- app/static/js/desktop/Bozuko/store/EntryTypes.js
+++ app/static/js/desktop/Bozuko/store/EntryTypes.js
@@ -5,20 +5,20 @@ Ext.define('Bozuko.store.EntryTypes', {
     fields : ['img','title','description','type','options','hidden'],
         
     data : [{
-        type: 'facebook/checkin',
-        title: 'Facebook Check in',
-        hidden: false,
-        img:'/images/desktop/app/builder/entry/facebook-checkin-fit.png',
-        description: [
-            '<p>A player must be present at your location and all entries post to player\'s walls.  Using the Bozuko app, players can "check in and play" with a single button.</p>'
-        ].join('')
-    },{
         type: 'facebook/like',
         title: 'Facebook Like',
         hidden: false,
         img:'/images/desktop/app/builder/entry/facebook-like-fit.png',
         description: [
             '<p>A player must first Facebook "Like" your business on in order to play the game.  The Bozuko application makes Liking your business easy by presenting the user an integrated Like button on your game page.</p>'
+        ].join('')
+    },{
+        type: 'facebook/checkin',
+        title: 'Facebook Check in',
+        hidden: false,
+        img:'/images/desktop/app/builder/entry/facebook-checkin-fit.png',
+        description: [
+            '<p>A player must be present at your location and all entries post to player\'s walls.  Using the Bozuko app, players can "check in and play" with a single button.</p>'
         ].join('')
     },{
         type: 'bozuko/checkin',
