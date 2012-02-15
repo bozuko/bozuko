758f21d5a557ecb4e9b4c54fe883723306eb07c2
Mark Fabrizio <mark.fabrizio@bozuko.com>
2012-02-15 02:09:07 +0000

    Ignore anything from phones.

--- app/middleware/redirection.js
+++ app/middleware/redirection.js
@@ -2,10 +2,13 @@ var burl = Bozuko.require('util/url').create;
 
 module.exports = function redirection(){
     return function redirection(req, res, next){
+        
         var host = Bozuko.cfg('server.host'),
             port = Bozuko.cfg('server.port'),
             header = req.header('host')
             ;
+        
+        if( req.param('mobile_version') ) return next();
         
         if( !~[443,80].indexOf(port) ) {
             host+=(':'+port);
