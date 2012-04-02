cd1f389213ea6cedee583c624feafe2a07be14bd
Mark Fabrizio <mark.fabrizio@bozuko.com>
2012-04-02 18:32:22 +0000

    Force refresh

--- app/static/js/client/game/Abstract.js
+++ app/static/js/client/game/Abstract.js
@@ -189,6 +189,7 @@ Bozuko.client.game.Abstract = Ext.extend( Ext.util.Observable, {
                             break;
                         
                         case 'facebook/liked':
+                            alert('hello!');
                             self.app.api.call(self.app.user.links.user, function(result){
                                 self.updateState(true);
                             });
