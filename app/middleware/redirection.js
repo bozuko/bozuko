3820b1f3507129c2d06f0a6c4b05d0fc1b9cdd3e
Mark Fabrizio <mark.fabrizio@bozuko.com>
2012-05-09 14:00:01 +0000

    Allow for urls that have ports for ssl to be allowed without redirect

--- app/middleware/redirection.js
+++ app/middleware/redirection.js
@@ -3,17 +3,21 @@ var burl = Bozuko.require('util/url').create;
 module.exports = function redirection(){
     return function redirection(req, res, next){
         
-        var host = Bozuko.cfg('server.host'),
-            port = Bozuko.cfg('server.port'),
-            header = req.header('host')
-            ;
+        var host = Bozuko.cfg('server.host')
+          , port = Bozuko.cfg('server.port')
+          , header = req.header('host')
+          , host_alt
+          ;
         
         if( req.param('mobile_version') ) return next();
         
         if( !~[443,80].indexOf(port) ) {
             host+=(':'+port);
         }
-        if( header && header != host ){
+        else{
+            host_alt+=(':'+port);
+        }
+        if( header && header != host && header != host_alt ){
             return res.redirect(burl(req.url));
         }
         return next();
