56b4ffa3ebf4b77b90f30d83031fb5cc108daa2f
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2012-05-22 04:31:19 +0000

    finish up codes pdf

--- app/util/codePdf.js
+++ app/util/codePdf.js
@@ -37,20 +37,30 @@ exports.createPdf = createPdf;
 function getContest(contest_id, callback) {
   Bozuko.models.Contest.findById(
       contest_id, 
-      {name: 1, start: 1, end: 1, results: 1, engine_type: 1},
+      {name: 1, start: 1, end: 1, results: 1, engine_type: 1, prizes: 1},
       callback);
 }
 
 function getCodes(contest, callback) {
   var codes = [];
+  for (var i = 0; i < contest.prizes.length; i++) {
+      codes[i] = [];
+  }
   if (contest.engine_type === 'order') {
     Object.keys(contest.results).forEach(function(key) {
       var code = contest.results[key].code;
-      if (code) codes.push(code);
+      var index = contest.results[key].index;
+      if (code) codes[index].push(code);
     });
     return callback(null, codes);
   } 
-  Bozuko.models.Result.find({contest_id: contest._id}, {code:1}, callback);
+  Bozuko.models.Result.find({contest_id: contest._id}, {code:1, index: 1}, function(err, results) {
+      if (err) return callback(err);
+      if (!results) return callback(new Error('No Codes Found!'));
+      results.forEach(function(result) {
+         codes[result.index].push(result.code); 
+      });
+  });
 }
 
 function createPdf(contest, codes, callback) {
@@ -58,14 +68,20 @@ function createPdf(contest, codes, callback) {
   doc.info.Title = contest.name;
   doc.registerFont('Bozuko',Bozuko.dir+'/resources/fonts/arvo/Arvo-Regular.ttf','ArvoRegular');
 
-  var header = contest.name + ': '+contest.start+' - '+contest.end;
+  var header = contest.name + '\n' +contest.start.toDateString()+' - '+contest.end.toDateString();
   doc.fill('#D3D3D3').font('Bozuko').fontSize(24).fill('black').text(header);
   doc.moveDown();
 
-  codes.forEach(function(code) {
-    doc.fill('#D3D3D3').font('Bozuko').fontSize(20).fill('black').text(code);
-    doc.moveDown();
-  });
+  for (var i = 0; i < contest.prizes.length; i++) {
+      var prize = contest.prizes[i].name;
+      doc.fill('#D3D3D3').font('Bozuko').fontSize(20).fill('black').text(prize);
+      doc.moveDown();
+      codes[i].sort();
+      codes[i].forEach(function(code) {
+        doc.fill('#D3D3D3').font('Bozuko').fontSize(16).fill('black').text(code);
+//        doc.moveDown();
+      });
+  }
 
   callback(null, doc);
 }
