54b4f895d74d762f1d05641e1ef72ac521ab9b74
Mark Fabrizio <mark.fabrizio@bozuko.com>
2012-05-30 22:37:30 +0000

    Hack to fix chrome on mac

--- app/static/js/client/App.js
+++ app/static/js/client/App.js
@@ -29,7 +29,6 @@ Bozuko.client.App = Ext.extend( Ext.util.Observable, {
                 alert(result.data.message);
                 if( self.scratch ) self.scratch.updateActionFromState();
             }
-            
         });
         
         this.createElements();
@@ -109,6 +108,16 @@ Bozuko.client.App = Ext.extend( Ext.util.Observable, {
     },
     
     createElements : function(){
+        
+        // hack to fix mac / chrome
+        var gpuCanvas = document.createElement('canvas');
+			gpuCanvas.width = gpuCanvas.height = 1;
+			gpuCanvas.getContext("experimental-webgl");
+            
+        gpuCanvas.style.position='absolute';
+        gpuCanvas.style.top='-100px';
+        gpuCanvas.style.left='-100px';
+        document.body.appendChild( gpuCanvas );
         
         this.$body = Ext.get(this.ct).createChild({
             cls         :'app'
