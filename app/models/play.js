90504f63a028eb5131402b54ebb885c46f37aa19
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2012-06-28 03:42:35 +0000

    remove potentially unused indexes and add a compound index

--- app/models/play.js
+++ app/models/play.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose'),
     ObjectId = Schema.ObjectId;
 
 var Play = module.exports = new Schema({
-    contest_id              :{type:ObjectId,        index: true},
-    page_id                 :{type:ObjectId,        index: true},
-    user_id                 :{type:ObjectId,        index: true},
+    contest_id              :{type:ObjectId},
+    page_id                 :{type:ObjectId},
+    user_id                 :{type:ObjectId},
     entry_id                :{type:ObjectId},
     uuid                    :{type:String},
     play_cursor             :{type:Number},
-    timestamp               :{type:Date,            index: true},
+    timestamp               :{type:Date},
     game                    :{type:String},
     win                     :{type:Boolean},
     free_play               :{type:Boolean},
@@ -19,3 +19,5 @@ var Play = module.exports = new Schema({
     consolation_prize_id    :{type:ObjectId},
     consolation_prize_name  :{type:String,          default: false}
 },{safe: {j:true}});
+
+Play.index({contest_id: 1, user_id: 1, timestamp: -1});
