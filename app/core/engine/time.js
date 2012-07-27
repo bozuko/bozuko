8c498e9ec8ab0a0daebc70ed44c53d5446a34eac
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2012-07-27 08:11:17 +0000

    move db calls for play and redistribution back into time engine and out of contest model

--- app/core/engine/time.js
+++ app/core/engine/time.js
@@ -25,34 +25,45 @@ inherits(TimeEngine, Engine);
 
 var lookback_window_floor = 1000*60*3;
 
-TimeEngine.prototype.configure = function(opts) {
+TimeEngine.prototype.setOpts = function(opts) {
+    var self = this;
     if (!opts) {
         opts = defaults;
     } else {
         opts = merge(opts, defaults);
     }
-    var self = this;
     Object.keys(opts).forEach(function(key) {
-        console.log(key);
         self[key] = opts[key];
     });
+};
+
+TimeEngine.prototype.configure = function(opts) {
+    this.setOpts(opts);
+
+    var totalContestLength = this.contest.end.getTime() - this.contest.start.getTime();
+
     // Leave a buffer at the end so users can always win the last prizes.
-    this.contest_duration = Math.floor(
-        (this.contest.end.getTime() - this.contest.start.getTime())*(1-this.buffer));
+    this.contest_duration = Math.floor(totalContestLength*(1-this.buffer));
 
     this.buffer_start = new Date(this.contest.start.getTime() + this.contest_duration);
-    this.lookback_threshold_start = new Date(this.contest.end.getTime() - 
-        Math.floor((this.contest.end.getTime() - this.contest.start.getTime())*this.lookback_threshold));
-    this.step =  Math.floor(
-        (this.contest_duration)/this.contest.totalPrizes());
+    
+    this.lookback_threshold_start = new Date(
+        this.contest.end.getTime() - Math.floor(totalContestLength*this.lookback_threshold)
+    );
+
+    this.step =  Math.floor(this.contest_duration/this.contest.totalPrizes());
+
     this.lookback_window = Math.round(this.step/this.window_divisor);
     if (this.lookback_window < lookback_window_floor) {
         this.lookback_window = lookback_window_floor;
     }
+
     this.throwahead_window = Math.round(this.step*this.throwahead_multiplier);
+
     if (this.contest.free_play_pct) {
         this.free_play_odds = Math.round(1/(this.contest.free_play_pct/100));
     }
+
     console.log('opts = ', opts);
     console.log("throwahead window = "+this.throwahead_window);
     console.log("lookback_window = "+this.lookback_window);
@@ -61,10 +72,95 @@ TimeEngine.prototype.configure = function(opts) {
     console.log("multiplay = "+this.multiplay);
 };
 
+TimeEngine.prototype.play = function(memo, callback) {
+    var now = memo.timestamp.getTime();
+    memo.max_lookback = new Date(now - this.lookback_window);
+
+    if (this.isFreePlay()) {
+        memo.result = 'free_play';
+        return callback(null, memo);
+    }
+
+    if (this.multiplay) {
+        if (failEarly(memo.entry.tokens)) return callback(null, memo);
+    }
+
+    memo.query = this.getPlayQuery(memo);
+
+    this.incrementPlayCursorForAnalytics();
+
+    this.getResult(memo, callback);
+};
+
+TimeEngine.prototype.getResult = function(memo, callback) {
+    var self = this;
+    return Bozuko.models.Result.findAndModify(
+        memo.query,
+        [['timestamp', 'asc']],
+        {$set: {win_time: memo.timestamp, user_id: memo.user._id, entry_id: memo.entry._id}},
+        {new: true, safe: safe},
+        function(err, result) {
+            if (err) return callback(err);
+            memo.result = result;
+            if (memo.result) return callback(null, memo);
+
+            self.redistribute(memo, callback);
+        }
+    );
+};
+
+TimeEngine.prototype.redistribute = function(memo, callback) {
+    var self = this;
+    var start = memo.timestamp.getTime();
+    var end = start + this.throwahead_window;
+    var new_time = new Date(rand(start, end));
+
+    // Don't redistribute into the buffer
+    if (new_time.getTime() > this.buffer_start.getTime()) return callback(null, memo);
+
+    memo.new_time = new_time;
+
+    this.redistributeResult(memo, callback);
+};
+
+TimeEngine.prototype.redistributeResult = function(memo, callback) {
+    var self = this;
+    return Bozuko.models.Result.findAndModify(
+        {contest_id: this.contest._id, win_time: {$exists: false}, timestamp: {$lt: memo.max_lookback}},
+        [['timestamp', 'asc']],
+        {$push: {history: {timestamp: memo.new_time, move_time: memo.timestamp}}, $set: {timestamp: memo.new_time}},
+        {new: false, safe: safe},
+            function(err, result) {
+            if (err) return callback(err);
+            if (result) {
+                console.log("contest: "+self.contest._id+" timestamp redistributed from "+
+                    result.timestamp+" to "+memo.new_time+" at "+memo.timestamp);
+
+                self.incrementRedistributionsForAnalytics();
+            }
+            return callback(null, memo);
+        }
+    );
+};
+
+TimeEngine.prototype.incrementRedistributionsForAnalytics = function(contest) {
+    var self = this;
+    Bozuko.models.Contest.update(
+        {_id: self.contest._id},
+        {$inc: {redistributions: 1}},
+        function(err) {
+            if (err) console.error('Redistribution analytics error: '+err);
+            else Bozuko.publish('contest/redistribute', {
+                contest_id: self.contest._id,
+                contest_name: self.contest.name,
+                restributions: self.contest.redistributions
+            });
+        }
+    );
+};
 
 TimeEngine.prototype.generateResults = function(Page, page_id, callback) {
     var self = this;
-    var options = this.contest.engine_options || {};
     var contest = this.contest;
     var prizes = contest.prizes;
     var start = contest.start.getTime();
@@ -79,10 +175,7 @@ TimeEngine.prototype.generateResults = function(Page, page_id, callback) {
             } else {
                 self.distributeInterval(contest._id, prize.total, prize_index, start, end, prefix, block, cb);
             }
-        }, function(err) {
-            if (err) return callback(err);
-            return contest.save(callback);
-        });
+        }, callback);
     });
 };
 
@@ -165,7 +258,7 @@ TimeEngine.prototype.enter = function(tokens, callback) {
         { _id: this.contest._id},
         [],
         {$inc : {'token_cursor': tokens}},
-        {new: true, fields: {_id:1}, safe: {w:2, wtimeout:5000}},
+        {new: true, fields: {_id:1}, safe: safe},
         function(err, contest) {
             // this is just for analytics
         }
@@ -183,68 +276,46 @@ TimeEngine.failEarly = function(tokens) {
     return true;
 };
 
-
-/**
- * Play an entry token
- *
- * @public
- */
-TimeEngine.prototype.play = function(memo, callback) {
-    var self = this;
-    var now = memo.timestamp.getTime();
-    var max_lookback = new Date(now - this.lookback_window);
-    memo.max_lookback = max_lookback;
-
-    // check for a free play before actually checking for win/loss
+TimeEngine.prototype.isFreePlay = function() {
     if (this.free_play_odds) {
         var winning_number = 1;
         if (rand(1, this.free_play_odds) === winning_number) {
-            memo.result = 'free_play';
-            return callback(null, memo);
+            return true;
         }
     }
+    return false;
+}
 
-    if (this.multiplay) {
-        if (failEarly(memo.entry.tokens)) return callback(null, memo);
-    }
-
-    // If we are into the buffer region, then allow wins on any previous prize.
-    // In other words, disregard the lookback window. We do this so that all prizes
-    // get handed out before the contest expires.
-    //
+// If we are into the buffer region, then allow wins on any previous prize.
+// In other words, disregard the lookback window. We do this so that all prizes
+// get handed out before the contest expires.
+//
+TimeEngine.prototype.getPlayQuery = function(memo) {
+    var now = memo.timestamp.getTime();
     if (now > this.lookback_threshold_start.getTime()) {
-        memo.query = self.contest.noLookbackQuery(memo);
+        return { 
+            contest_id: this.contest._id,
+            timestamp: {$lte: memo.timestamp},
+            win_time: {$exists: false}
+        };
     } else {
-        memo.query = self.contest.lookbackQuery(memo);
+        return {
+            contest_id: this.contest._id,
+            $and: [{timestamp: {$gt: memo.max_lookback}}, {timestamp: {$lte: memo.timestamp}}],
+            win_time: {$exists: false}
+        };
     }
+};
 
+TimeEngine.prototype.incrementPlayCursorForAnalytics = function() {
     Bozuko.models.Contest.findAndModify(
         {_id: this.contest._id},
         [],
         {$inc : {play_cursor: 1}},
-        {new: true, fields: {_id: 1}, safe: {w:2, wtimeout:5000}},
+        {new: true, fields: {_id: 1}},
         function(err, contest) {
             // this is just for analytics
         }
     );
-
-    self.contest.getTimeResult(memo, callback);
-};
-
-
-/**
- * Redistribute results if they were not won and are prior to the lookback window
- *
- */
-TimeEngine.prototype.redistribute = function(old_time) {
-    var self = this;
-    var start = old_time.getTime();
-    var end = start + this.throwahead_window;
-    var new_time = new Date(rand(start, end));
-
-    // Don't redistribute into the buffer
-    if (new_time.getTime() > this.buffer_start.getTime()) return null;
-
-    return new_time;
 };
 
