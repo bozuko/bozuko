964f3978f74b50adb75d658cbbb3394af2db8669
Mark Fabrizio <mark.fabrizio@bozuko.com>
2012-10-09 01:06:17 +0000

    Fix typo

--- app/util/url.js
+++ app/util/url.js
@@ -4,7 +4,7 @@ module.exports = {
         
         path = !path ? '' : (path[0]!='/'?'/'+path:path);
         
-        var port = ~[443,80].indexOf(Bozuko.config.server.port) ?
+        var port = !~[443,80].indexOf(Number(Bozuko.config.server.port)) ?
             (':'+Bozuko.config.server.port) :
             '';
         
