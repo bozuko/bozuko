662803351f82b525b172d2ebdd0c752103b08bed
Mark Fabrizio <mark.fabrizio@bozuko.com>
2012-10-09 01:05:29 +0000

    Remove the unecessary ports

--- app/util/url.js
+++ app/util/url.js
@@ -3,12 +3,16 @@ module.exports = {
     create: function(path){
         
         path = !path ? '' : (path[0]!='/'?'/'+path:path);
+        
+        var port = ~[443,80].indexOf(Bozuko.config.server.port) ?
+            (':'+Bozuko.config.server.port) :
+            '';
+        
         var parts = [
             Bozuko.config.server.ssl?'https:':'http:',
             '//',
             Bozuko.config.server.host,
-            ':',
-            Bozuko.config.server.port,
+            port,
             path
         ];
         return parts.join('');
