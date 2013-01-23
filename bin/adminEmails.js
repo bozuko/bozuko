cc0576d1deb1ba8923f12fedc39e1ef4e4cc65e3
Mark Fabrizio <mark.fabrizio@bozuko.com>
2013-01-23 14:09:59 +0000

    Query the database for unique entries See the comment for reasoning

--- bin/adminEmails.js
+++ bin/adminEmails.js
@@ -2,22 +2,54 @@ var bozuko = require('../app/bozuko');
 var mail = require('../app/util/mail');
 var async = require('async');
 
+var getUniqueCount = function( contest, cb ){
+    
+    /**
+     * I am going back to querying the database here because for some reason the
+     * counts are way off. Since this only runs once per day, and we don't have
+     * a ton of contests running at the same time right now, I don't think the
+     * performance hit will be that bad - its the same as just hitting admin.
+     * If anything, maybe we space out each email so we aren't pounding with these
+     * queries in quick succession.
+     */
+    
+    // if we wanted to flip it back to just looking at the counter, just use:
+    //
+    // if( 1) return cb(null, contest.unique_users);
+    
+    var selector = {
+        contest_id: contest._id
+    };
+    Bozuko.models.Entry.distinct('user_id', selector, function(error, results){
+        console.log(results);
+        if( error ) return cb(error);
+        return cb(null, results.length);
+    });
+};
+
 var getActiveContests = function(callback) {
     Bozuko.models.Contest.find(
-        {start: {$lt: new Date()}, end: {$gt: new Date()}, active: true},
+        {start: {$lt: new Date()}, end: {$gt: new Date()}, active: true, admin_emails: {$exists:true, $ne: ''}, },
         {_id: 1, game_config: 1, admin_emails: 1, unique_users: 1},
         function(err, contests) {
             if (err) return callback(err);
             var objects = [];
-            contests.forEach(function(contest) {
-                objects.push({
-                    _id: contest._id,
-                    name: contest.game_config.name,
-                    admin_emails: contest.admin_emails,
-                    unique_users: contest.unique_users || 0
+            return async.forEach(contests, function(contest, cb) {
+                getUniqueCount(contest, function(error, unique_count){
+                    if( error ) return cb(error);
+                    objects.push({
+                        _id: contest._id,
+                        name: contest.game_config.name,
+                        admin_emails: contest.admin_emails,
+                        unique_users: unique_count || 0
+                    });
+                    return cb();
                 });
+            }, function(error){
+                if(error) console.log(error);
+                callback(error, objects);
             });
-            callback(null, objects);
+            
         }
     );
 };
