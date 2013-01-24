e3da7223400f56c30669c6f46ef67a7a294e6044
Mark Fabrizio <mark.fabrizio@bozuko.com>
2013-01-24 21:55:05 +0000

    Update cfa theme slot icon order

--- app/games/slots/themes/cfa/index.js
+++ app/games/slots/themes/cfa/index.js
@@ -2,7 +2,7 @@ var EasyTheme = require('./../../easytheme');
 
 var CFATheme = module.exports = EasyTheme.create({
     dirname     : __dirname,
-    order       :['money-icon','strawberry-icon','trophy-icon'],
+    order       :['StarCoin','strawberry-icon','money-icon', 'Number-7-icon'],
     icon        :'icons/money-icon.png',
     version     :1
 });
